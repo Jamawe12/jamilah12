240a84150d0a36adc9d447bdcaae871d95393b62
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2021-12-01 10:05:27 +0700

    Create apk2.js

---
+++ apk2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://accomplishplunderbreeches.com/a4n55kccv?key=79d2a46ce6a47a98dd0d0b0e5c8ea400',
+  'https://accomplishplunderbreeches.com/n08p52ksiy?key=6dba6664cd5a5be378882c5885db36fc',
+  'https://accomplishplunderbreeches.com/q5be6fig0?key=c47716009fdd761344de6b9e69942465'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
