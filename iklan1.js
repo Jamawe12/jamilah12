0a1558b5514e532442e062ed831008ca987a41ea
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-01-13 19:43:59 +0700

    Create iklan1.js

---
+++ iklan1.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://protectorincorporatehush.com/z6sdeebcu?key=a28d5317b704441c0e341f8ecd8d8635',
+  'https://protectorincorporatehush.com/nsffbwau02?key=9daa7fd3937cee5c706c3a1c6b1f1fa5',
+  'https://protectorincorporatehush.com/eqe18p1zh?key=016a79046d31b2b0b8e0a5829307ff01'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
