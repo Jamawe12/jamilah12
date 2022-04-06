ad2e54dd0a2e9a36ae78a619c054a49810d9cf61
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-04-06 14:21:52 +0700

    Create blog1.js

---
+++ blog1.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+  
+  'https://conversisave.blogspot.com/2022/02/blog-post.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
