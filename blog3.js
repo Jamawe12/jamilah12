5ffe1bc9387d1250f8964bf6c397e93904f1817f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-04-09 15:04:46 +0700

    Create blog3.js

---
+++ blog3.js
@@ -0,0 +1,35 @@
+var dataUrl = [
+  
+  'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_15.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_14.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whtsapp_13.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_12.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_11.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_10.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_9.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_8.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_7.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_6.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_5.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whtsapp.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_3.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp_2.html',
+
+'https://kuningwarna.blogspot.com/2021/10/grup-whatsapp.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
