50cac92ef643799ad08bcd390970b941c0b9c5e4
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-04-18 00:29:21 +0700

    Create wa1.js

---
+++ wa1.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+  
+  'https://kuningwarna.blogspot.com/2022/04/grup-whatsapp.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
