1e7a97580b157e9602a11c47476b9e7c43a5dfad
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-05-10 14:09:16 +0700

    Delete apk1.js

--- apk1.js
+++
@@ -1,7 +0,0 @@
-var dataUrl = [
-  
-  'https://pjjpp.com/fullpage.php?section=General&pub=757822&ga=g'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
