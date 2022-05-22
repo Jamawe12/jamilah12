bec4821aa78bd7a34a8c59f5e7e1f7506f6ae364
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-05-22 16:16:04 +0700

    Create videoplus4.js

---
+++ videoplus4.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+  
+  'https://berbagivideoplus.blogspot.com/2022/05/video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
