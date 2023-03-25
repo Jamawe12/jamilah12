cf410d066b2fba77101b6aa3b744cf4566447a7f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-03-25 19:52:04 +0700

    Create cerita12.js

---
+++ cerita12.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://practicallysacrificestock.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4',
+
+  'https://practicallysacrificestock.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4',
+
+  'https://practicallysacrificestock.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
