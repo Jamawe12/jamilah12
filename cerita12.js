10273796c9376312091f07cc6725dd2979f10d19
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-06-05 11:41:06 +0700

    Update cerita12.js

--- cerita12.js
+++ cerita12.js
@@ -1,10 +1,10 @@
 var dataUrl = [
   
-  'https://practicallysacrificestock.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4',
+  'https://againboundless.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4',
 
-  'https://practicallysacrificestock.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4',
+  'https://againboundless.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4',
 
-  'https://practicallysacrificestock.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4'
+  'https://againboundless.com/n8css71fg?key=8d20b5ece066705379aa7221f185bba4'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
