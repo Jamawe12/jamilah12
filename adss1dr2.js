520978a45c50510cc10d52f040b2d5477c0ccf8a
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-06-10 21:36:51 +0700

    Create adss1dr2.js

---
+++ adss1dr2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://againboundless.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358',
+  'https://againboundless.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358',
+  'https://againboundless.com/zwq4zqn7x?key=4a867a0e416c4bade143b800341f8358'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
