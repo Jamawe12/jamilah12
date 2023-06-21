df092de5f85d999bfc0cd6eb0a88caf567f1c8d3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-06-21 13:55:43 +0700

    Create videocinema21.js

---
+++ videocinema21.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://ceritavideo12.blogspot.com/',
+
+  'https://ceritavideo12.blogspot.com/',
+
+  'https://ceritavideo12.blogspot.com/'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
