50f2d4ff1823da222d2b185e5eb69f8e03172659
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-09-20 15:11:32 +0700

    Create blogdr1ads2.js

---
+++ blogdr1ads2.js
@@ -0,0 +1,14 @@
+var dataUrl = [
+
+  'https://videofullhdn.blogspot.com/2023/09/video_83.html',
+
+'https://videofullhdn.blogspot.com/2023/09/video_69.html',
+
+'https://videofullhdn.blogspot.com/2023/09/video_88.html',
+
+'https://videofullhdn.blogspot.com/2023/09/video_19.html',
+  'https://videofullhdn.blogspot.com/2023/09/video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
