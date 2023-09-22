3d8fe0c74f10d1493bce7e03527670451cbd03ce
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-09-22 09:43:53 +0700

    Update ads2drl1test.js

--- ads2drl1test.js
+++ ads2drl1test.js
@@ -2,11 +2,12 @@ var dataUrl = [
 
   'https://kertastisubiru.blogspot.com/2023/09/video_22.html',
 
-'https://kertastisubiru.blogspot.com/2023/09/video_66.html',
+  'https://kertastisubiru.blogspot.com/2023/09/video_66.html',
 
-'https://kertastisubiru.blogspot.com/2023/09/video_77.html',
+  'https://kertastisubiru.blogspot.com/2023/09/video_77.html',
 
-'https://kertastisubiru.blogspot.com/2023/09/video_21.html',
+  'https://kertastisubiru.blogspot.com/2023/09/video_21.html',
+  
   'https://kertastisubiru.blogspot.com/2023/09/video.html'
 
 ];
