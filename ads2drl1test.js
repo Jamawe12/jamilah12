1234ed3ee3fda0c512f2e66c424ef47f85c1b8c7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-09-22 09:42:12 +0700

    Update and rename wa1.js to ads2drl1test.js

---
+++ ads2drl1test.js
@@ -0,0 +1,14 @@
+var dataUrl = [
+
+  'https://kertastisubiru.blogspot.com/2023/09/video_22.html',
+
+'https://kertastisubiru.blogspot.com/2023/09/video_66.html',
+
+'https://kertastisubiru.blogspot.com/2023/09/video_77.html',
+
+'https://kertastisubiru.blogspot.com/2023/09/video_21.html',
+  'https://kertastisubiru.blogspot.com/2023/09/video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
--- wa1.js
+++
@@ -1,7 +0,0 @@
-var dataUrl = [
-  
-  'https://kuningwarna.blogspot.com/2022/04/grup-whatsapp.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
