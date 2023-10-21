948adcd3b1ced8c5ed50b23aaa60e286a79d5d3c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-10-21 09:45:49 +0700

    Update moneads1.js

--- moneads1.js
+++ moneads1.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'soocaips.com/4/5017098',
+  'https://soocaips.com/4/5017098',
 
-  'soocaips.com/4/5017098',
+  'https://soocaips.com/4/5017098',
 
-  'soocaips.com/4/5017098'
+  'https://soocaips.com/4/5017098'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
