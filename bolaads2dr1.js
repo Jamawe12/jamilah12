3f3e166223bea8b682d084cca2d8a6a6921d0341
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-11-13 10:57:34 +0700

    Create bolaads2dr1.js

---
+++ bolaads2dr1.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://shadytourdisgusted.com/g3m6dqyxkq?key=6ff5e42949c1c3113a85a225b2586b28',
+
+  'https://shadytourdisgusted.com/g3m6dqyxkq?key=6ff5e42949c1c3113a85a225b2586b28',
+
+  'https://shadytourdisgusted.com/g3m6dqyxkq?key=6ff5e42949c1c3113a85a225b2586b28'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
