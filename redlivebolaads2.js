1fd0562c567cbb60b327632171baf979411009b0
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-11-13 15:17:42 +0700

    Create redlivebolaads2.js

---
+++ redlivebolaads2.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://livetimnasu17.blogspot.com',
+
+  'https://livetimnasu17.blogspot.com',
+
+  'https://livetimnasu17.blogspot.com'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
