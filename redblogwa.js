2201bb54853ea2e81cff2ef6613e77c0e0a48e14
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-11-23 14:08:12 +0700

    Create redblogwa.js

---
+++ redblogwa.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://filmasikin.blogspot.com/2023/11/video.html',
+
+  'https://filmasikin.blogspot.com/2023/11/video.html',
+
+  'https://filmasikin.blogspot.com/2023/11/video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
