354d22111d4a495fef13d755f0417db3cf77615f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-12-16 20:01:32 +0700

    Create ads2drr6hadiah.js

---
+++ ads2drr6hadiah.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://hadiahgratisuntukkamu12.blogspot.com/2023/12/hadiah-gratis_71.html',
+
+  'https://hadiahgratisuntukkamu12.blogspot.com/2023/12/hadiah-gratis_16.html',
+
+  'https://hadiahgratisuntukkamu12.blogspot.com/2023/12/hadiah-gratis.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
