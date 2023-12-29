8e62cdfd948bff270675724ac078c801a1ba8339
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-12-29 17:48:47 +0700

    Update adst2k3.js

--- adst2k3.js
+++ adst2k3.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'https://abbotpredicateemma.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
 
-  'https://abbotpredicateemma.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
 
-  'https://abbotpredicateemma.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
+  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
