ffb395ee46a254dba1bfc47aac6b091504aca8d4
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2023-12-30 17:25:32 +0700

    Update ads2dr6.js

--- ads2dr6.js
+++ ads2dr6.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'https://shadytourdisgusted.com/j5ut1kjz4?key=631171763f1979aa53c4fe47312907a5',
+  'https://contestjoe.com/j5ut1kjz4?key=631171763f1979aa53c4fe47312907a5',
 
-  'https://shadytourdisgusted.com/j5ut1kjz4?key=631171763f1979aa53c4fe47312907a5',
+  'https://contestjoe.com/j5ut1kjz4?key=631171763f1979aa53c4fe47312907a5',
 
-  'https://shadytourdisgusted.com/j5ut1kjz4?key=631171763f1979aa53c4fe47312907a5'
+  'https://contestjoe.com/j5ut1kjz4?key=631171763f1979aa53c4fe47312907a5'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
