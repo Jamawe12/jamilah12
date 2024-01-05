e15e4b9dc890826151c229cc82d086a86ccc108f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-01-05 14:38:15 +0700

    Update ads2dr2.js

--- ads2dr2.js
+++ ads2dr2.js
@@ -1,6 +1,6 @@
 var dataUrl = [
   
-  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e588',
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
 
   'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
 
