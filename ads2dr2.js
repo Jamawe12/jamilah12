62cc89115853baebb5520aaa67835d2dbac7f701
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-01-05 14:34:42 +0700

    Update ads2dr2.js

--- ads2dr2.js
+++ ads2dr2.js
@@ -1,10 +1,10 @@
 var dataUrl = [
   
-  'https://moderategermmaria.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e588',
 
-  'https://moderategermmaria.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58',
 
-  'https://moderategermmaria.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58'
+  'https://shadytourdisgusted.com/grg4u3xe7?key=df80af62a9deca8a6d7b1d9630602e58'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
