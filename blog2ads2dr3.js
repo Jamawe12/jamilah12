3138b068481fe1a607b6789cb784b442ae84c2ac
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-01-09 14:45:06 +0700

    Create blog2ads2dr3.js

---
+++ blog2ads2dr3.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://filmasikin.blogspot.com/2024/01/play-video_32.html',
+
+  'https://filmasikin.blogspot.com/2024/01/play-video_8.html',
+
+  'https://filmasikin.blogspot.com/2024/01/play-video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
