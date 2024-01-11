2fef7c6afc8f4b73e04ff9e8c9944369817c259e
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-01-11 12:01:04 +0700

    Create blogrdtrendy1.js

---
+++ blogrdtrendy1.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://trendyvideohdd.blogspot.com/2024/01/mulus-pink-punya.html',
+
+  'https://trendyvideohdd.blogspot.com/2024/01/asupan-paling-gemes.html',
+
+  'https://trendyvideohdd.blogspot.com/2024/01/pemuas-ayang.html',
+
+  'https://trendyvideohdd.blogspot.com/2024/01/di-ajak-pacar-main-di-kamar-kos_10.html',
+
+  'https://trendyvideohdd.blogspot.com/'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
