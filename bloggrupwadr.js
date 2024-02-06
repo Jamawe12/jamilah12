b6467fb70f7f4e27b586a9fcce89d5ac8827c790
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-02-06 19:39:12 +0700

    Create bloggrupwadr.js

---
+++ bloggrupwadr.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+
+  'https://grupwadisiniya.blogspot.com/2024/02/grup-wa.html',
+
+  'https://grupwadisiniya.blogspot.com/2024/02/grup-wa_2.html',
+
+  'https://grupwadisiniya.blogspot.com/2024/02/grup-wa_3.html',
+
+  'https://grupwadisiniya.blogspot.com/2024/02/grup-wa_4.html',
+  
+  'https://grupwadisiniya.blogspot.com/2024/02/grup-wa_5.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
