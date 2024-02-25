25130deec8348bfd086c363b2d4e6eee8db13bfc
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-02-25 10:08:34 +0700

    Update adstdrblogre1.js

--- adstdrblogre1.js
+++ adstdrblogre1.js
@@ -4,7 +4,7 @@ var dataUrl = [
 
   'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_43.html',
 
-  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_14.html'
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_18.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
