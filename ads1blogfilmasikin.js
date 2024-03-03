6da3c381d8e3f8784ec4813bb2e8677a10d622db
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-03-03 09:40:38 +0700

    Update ads1blogfilmasikin.js

--- ads1blogfilmasikin.js
+++ ads1blogfilmasikin.js
@@ -1,14 +1,12 @@
 var dataUrl = [
   
-  'https://google.com',
+  'https://livebolafriendlyindo6.blogspot.com/2024/01/live-streaming-indonesia-vs-libya_5.html',
 
-  'https://google.com',
+  'https://livebolafriendlyindo6.blogspot.com/2024/01/live-streaming-indonesia-vs-libya.html',
 
-  'https://google.com',
+  'https://livebolafriendlyindo6.blogspot.com/2024/01/live-streaming-indonesia-vs-libya_5.html',
 
-  'https://google.com',
-
-  'https://google.com'
+  'https://livebolafriendlyindo6.blogspot.com/2024/01/live-streaming-indonesia-vs-libya.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
