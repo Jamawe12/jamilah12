795677da4c60f4f0fdacc94514ae0f2e8c17f292
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-05-16 20:05:20 +0700

    Create adstdrblogre1new.js

---
+++ adstdrblogre1new.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_17.html',
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_60.html',
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_89.html',
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_11.html',
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_99.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
