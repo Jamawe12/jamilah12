db997739faaacf7a7b1809a60edb8e476fe4a6e3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-05-16 17:29:45 +0700

    Update adstdrblogre1.js

--- adstdrblogre1.js
+++ adstdrblogre1.js
@@ -1,10 +1,14 @@
 var dataUrl = [
 
-  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_94.html',
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_17.html',
 
-  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_81.html',
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_60.html',
 
-  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_13.html'
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_89.html',
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_11.html',
+
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_99.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
