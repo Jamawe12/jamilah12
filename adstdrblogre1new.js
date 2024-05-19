9b21cd1ae4f9836831f05e9671c5837e0e266e64
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-05-19 08:49:48 +0700

    Update adstdrblogre1new.js

--- adstdrblogre1new.js
+++ adstdrblogre1new.js
@@ -1,12 +1,12 @@
 var dataUrl = [
+  
+  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_94.html',
 
   'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_17.html',
 
   'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_60.html',
 
   'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_89.html',
-
-  'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_11.html',
 
   'https://grupwadisinifull.blogspot.com/2024/01/grup-wa_99.html'
 
