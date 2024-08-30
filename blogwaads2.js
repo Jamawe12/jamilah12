974e911836472a6102cc6fe2b005360630bfeb85
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-08-30 09:20:55 +0700

    Update blogwaads2.js

--- blogwaads2.js
+++ blogwaads2.js
@@ -2,14 +2,10 @@ var dataUrl = [
   
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-1.html',
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-2.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-3.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-4.html',
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-5.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-6.html',
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-7.html',
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-8.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-9.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-10.html'
+  'https://grupwhatsapviral.blogspot.com/2024/08/grup-9.html'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
