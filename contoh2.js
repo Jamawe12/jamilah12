88318f286bc0e2bcf561f00c42f99d9f299b6c6a
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-08-30 10:26:08 +0700

    Create contoh2.js

---
+++ contoh2.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://xvideoasiaviral.blogspot.com/2024/08/play-video.html',
+
+  'https://xvideoasiaviral.blogspot.com/2024/08/play-video.html',
+
+  'https://xvideoasiaviral.blogspot.com/2024/08/play-video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
