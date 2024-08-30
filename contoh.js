f7db2640f4b600e0862ba002f621abc2f5a1c094
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-08-30 10:12:34 +0700

    Create contoh.js

---
+++ contoh.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://hdvideoonee.blogspot.com/2024/08/play-video.html',
+
+  'https://hdvideoonee.blogspot.com/2024/08/play-video.html',
+
+  'https://hdvideoonee.blogspot.com/2024/08/play-video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
