ff2a3a64415904be219d6b3a7050f770af452600
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 14:41:04 +0700

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var dataUrl = [
-  
-  'https://livebolaafcu23timnas4.blogspot.com/2024/04/live-indonesia-u23-vs-korea-selatan-u23.html',
-  'https://livebolaafcu23timnas4.blogspot.com/2024/04/live-korea-selatan-u23-vs-indonesia-u23.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
