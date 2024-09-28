0f7502a1b4b56f3d0b5dcf18d2a5cc9249940c8d
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 14:37:44 +0700

    Update adst2k3.js

--- adst2k3.js
+++ adst2k3.js
@@ -1,5 +1,9 @@
 var dataUrl = [
 
+  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+
+  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+
   'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
 
 ];
