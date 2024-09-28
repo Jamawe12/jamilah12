bb2b104c73e9ccb77683dd347e6460de07262dab
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 14:40:22 +0700

    Delete videoplus4.js

--- videoplus4.js
+++
@@ -1,7 +0,0 @@
-var dataUrl = [
-  
-  'https://berbagivideoplus.blogspot.com/2022/05/video.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
