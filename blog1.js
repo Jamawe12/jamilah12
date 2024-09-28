7e5c62773d4a08c59b8cc3e677a32de5452f20a7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 15:00:37 +0700

    Delete blog1.js

--- blog1.js
+++
@@ -1,7 +0,0 @@
-var dataUrl = [
-  
-  'https://pilihpilihanmu22.blogspot.com/2023/12/pilih-pilihanmu_28.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
