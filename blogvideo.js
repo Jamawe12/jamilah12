f5a876bd4bdf816a5144c24853bb74cbb6a3ffd6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 14:44:16 +0700

    Delete blogvideo.js

--- blogvideo.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-  
-  'https://bukukuningbiru.blogspot.com',
-  'https://bukukuningbiru.blogspot.com',
-  'https://bukukuningbiru.blogspot.com'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
