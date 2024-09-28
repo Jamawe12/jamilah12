5dec1e215d6ffd55de6d32c9fc263de72bebdb1e
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 14:43:18 +0700

    Delete bolablogdr1.js

--- bolablogdr1.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-  
-  'https://livebolapildun.blogspot.com/2023/11/live-streaming-piala-dunia-u17_21.html',
-  'https://livebolapildun.blogspot.com/2023/11/live-streaming-piala-dunia-u17_20.html',
-  'https://livebolapildun.blogspot.com/2023/11/live-streaming-piala-dunia-u17.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
