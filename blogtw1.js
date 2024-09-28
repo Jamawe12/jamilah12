34eab9a12fd42e077c1ba0fb1e23dfbc789f6be3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 14:44:28 +0700

    Delete blogtw1.js

--- blogtw1.js
+++
@@ -1,7 +0,0 @@
-var dataUrl = [
-
-'https://practicallysacrificestock.com/y508wur2?key=e57dc7d25ee44cdcbaf224d25522fa21'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
