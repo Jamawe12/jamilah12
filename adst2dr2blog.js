9b5be7af772a2ccb6b7ac62d36119fd63b2a73d9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 15:03:59 +0700

    Delete adst2dr2blog.js

--- adst2dr2blog.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-  
-  'https://videoo2121.blogspot.com/2023/11/video_3.html',
-  'https://videoo2121.blogspot.com/2023/11/video.html',
-  'https://videoo2121.blogspot.com/2023/10/video_99.html'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
