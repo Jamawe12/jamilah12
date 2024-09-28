f846accb81c76b3c026b044258447f1004547b55
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-28 15:16:04 +0700

    Create blogwa1.js

---
+++ blogwa1.js
@@ -0,0 +1,14 @@
+var dataUrl = [
+  
+  'https://grupwhatsapviral.blogspot.com/2024/08/grup-2.html',
+  'https://grupwhatsapviral.blogspot.com/2024/08/grup-5.html',
+  'https://grupwhatsapviral.blogspot.com/2024/08/grup-7.html',
+  'https://grupwhatsapviral.blogspot.com/2024/08/grup-8.html',
+  'https://grupwhatsapviral.blogspot.com/2024/09/grup-11.html',
+  'https://grupwhatsapviral.blogspot.com/2024/09/grup-12.html',
+  'https://grupwhatsapviral.blogspot.com/2024/09/grup-13.html',
+  'https://grupwhatsapviral.blogspot.com/2024/09/grup-14.html',
+  'https://grupwhatsapviral.blogspot.com/2024/09/grup-15.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
