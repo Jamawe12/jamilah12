a294d0a06d676a211dab363722319a24195c3b64
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-09-30 10:19:32 +0700

    Update blogwa1.js

--- blogwa1.js
+++ blogwa1.js
@@ -1,8 +1,6 @@
 var dataUrl = [
   
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-2.html',
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-5.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-7.html',
   'https://grupwhatsapviral.blogspot.com/2024/08/grup-8.html',
   'https://grupwhatsapviral.blogspot.com/2024/09/grup-11.html',
   'https://grupwhatsapviral.blogspot.com/2024/09/grup-12.html',
