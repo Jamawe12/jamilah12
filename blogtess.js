4e1c26d4c3d8f1f0e1381db15505852dfca3ea50
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-10-09 10:23:23 +0700

    Create blogtess.js

---
+++ blogtess.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://blogtesrisett.blogspot.com/2024/10/blog-1.html',
+
+  'https://blogtesrisett.blogspot.com/2024/10/blog-1.html',
+
+  'https://blogtesrisett.blogspot.com/2024/10/blog-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
