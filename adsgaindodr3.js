ede74313b38a2583e6ba1c618c7df4b7f4f7ac80
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-10-28 11:32:55 +0700

    Create adsgaindodr3.js

---
+++ adsgaindodr3.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://www.profitablecpmrate.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f',
+
+  'https://www.profitablecpmrate.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f',
+
+  'https://www.profitablecpmrate.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
