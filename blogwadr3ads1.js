ee9feb888c5590b8201d547ac2927d315e354bff
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-11-01 16:36:16 +0700

    Create blogwadr3ads1.js

---
+++ blogwadr3ads1.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://grupwhatsappviral66.blogspot.com/2024/09/grup-1.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/09/grup-2.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-3.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-4.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-5.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-6.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-7.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-8.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-9.html',
+  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-10.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
