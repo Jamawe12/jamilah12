fccd9b5bf4b1046a6224e29a03da113eea8f5db7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-11-01 16:34:36 +0700

    Delete blogwa1.js

--- blogwa1.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-  
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-5.html',
-  'https://grupwhatsapviral.blogspot.com/2024/08/grup-8.html',
-  'https://grupwhatsapviral.blogspot.com/2024/09/grup-15.html',
-  'https://grupwhatsapviral.blogspot.com/2024/10/grup-16.html'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
