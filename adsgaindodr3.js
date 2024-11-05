b6b89f97b91d51c2b8d28fe51c4d599c94464f9b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-11-05 22:43:48 +0700

    Update adsgaindodr3.js

--- adsgaindodr3.js
+++ adsgaindodr3.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'https://www.profitablecpmrate.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f',
+  'https://slickquiver.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f',
 
-  'https://www.profitablecpmrate.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f',
+  'https://slickquiver.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f',
 
-  'https://www.profitablecpmrate.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f'
+  'https://slickquiver.com/k96b9uzsnv?key=5aa2c0ef885ea103a11b423b4dc8f25f'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
