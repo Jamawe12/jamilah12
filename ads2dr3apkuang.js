a8c8b113b8c1822d3f6d851c7cc095c416c27ea2
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2024-11-30 16:41:57 +0700

    Create ads2dr3apkuang.js

---
+++ ads2dr3apkuang.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://apkpenghasilcuan12.blogspot.com/2024/11/apk-penghasil-uang-1.html',
+  'https://apkpenghasilcuan12.blogspot.com/2024/11/apk-penghasil-uang-2.html',
+  'https://apkpenghasilcuan12.blogspot.com/2024/11/apk-penghasil-uang-3.html',
+  'https://apkpenghasilcuan12.blogspot.com/2024/11/apk-penghasil-uang-4.html',
+  'https://apkpenghasilcuan12.blogspot.com/2024/11/apk-penghasil-uang-5.html'
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
