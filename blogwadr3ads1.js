38f4454d53a1e7a2e5c2512d3e6f6e3ec49fee65
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-01-12 14:18:43 +0700

    Update blogwadr3ads1.js

--- blogwadr3ads1.js
+++ blogwadr3ads1.js
@@ -4,7 +4,6 @@ var dataUrl = [
   'https://grupwhatsappviral66.blogspot.com/2024/09/grup-2.html',
   'https://grupwhatsappviral66.blogspot.com/2024/11/grup-3.html',
   'https://grupwhatsappviral66.blogspot.com/2024/11/grup-4.html',
-  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-5.html',
   'https://grupwhatsappviral66.blogspot.com/2024/11/grup-7.html',
   'https://grupwhatsappviral66.blogspot.com/2024/11/grup-8.html',
   'https://grupwhatsappviral66.blogspot.com/2024/11/grup-9.html'
