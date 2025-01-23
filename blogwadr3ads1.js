457c5e59f558cb56cfda9650aed98954fa996627
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-01-23 16:58:20 +0700

    Update blogwadr3ads1.js

--- blogwadr3ads1.js
+++ blogwadr3ads1.js
@@ -1,11 +1,15 @@
 var dataUrl = [
   
-  'https://grupwhatsappviral66.blogspot.com/2024/09/grup-2.html',
-  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-3.html',
-  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-4.html',
-  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-7.html',
-  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-8.html',
-  'https://grupwhatsappviral66.blogspot.com/2024/11/grup-9.html'
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-11.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-12.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-13.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-14.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-15.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-16.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-17.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-18.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-19.html',
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-20.html'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
