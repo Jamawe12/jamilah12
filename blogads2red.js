902dd434a182dce796e15bc10ced63347940cf7d
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-01-30 13:28:56 +0700

    Create blogads2red.js

---
+++ blogads2red.js
@@ -0,0 +1,23 @@
+var dataUrl = [
+  
+  'https://berbagigrupwaviral11.blogspot.com/2025/01/grup-1.html',
+  'https://berbagigrupwaviral11.blogspot.com/2025/01/grup-2.html',
+  'https://berbagigrupwaviral11.blogspot.com/2025/01/grup-3.html',
+  'https://berbagigrupwaviral11.blogspot.com/2025/01/grup-4.html',
+  'https://berbagigrupwaviral11.blogspot.com/2025/01/grup-5.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+
+
+
+
+
+
+
+
+
+
+
+
+
