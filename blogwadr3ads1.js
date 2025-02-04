41e21445378ce1d6481b0e1ce029bf5a11619cfb
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-02-04 10:22:43 +0700

    Update blogwadr3ads1.js

--- blogwadr3ads1.js
+++ blogwadr3ads1.js
@@ -6,8 +6,7 @@ var dataUrl = [
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-14.html',
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-15.html',
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-16.html',
-  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-18.html',
-  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-20.html'
+  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-18.html'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
