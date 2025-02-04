b9056ebaf1ec2a4addde8c757b604d3c0197e475
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-02-04 10:23:31 +0700

    Update blogwadr3ads1baru.js

--- blogwadr3ads1baru.js
+++ blogwadr3ads1baru.js
@@ -4,7 +4,6 @@ var dataUrl = [
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-12.html',
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-13.html',
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-14.html',
-  'https://grupwhatsappviral66.blogspot.com/2025/01/grup-15.html',
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-16.html',
   'https://grupwhatsappviral66.blogspot.com/2025/01/grup-18.html'
 ];
