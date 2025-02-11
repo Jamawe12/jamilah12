0a7c6c88a8f91df0d62e480a913c5da557e54b07
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-02-11 11:48:06 +0700

    Create blogwaadst1dr3red.js

---
+++ blogwaadst1dr3red.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-1.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/05/grup-2.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-3.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/05/grup-4.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-5.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-6.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-7.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-8.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-9.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-10.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
