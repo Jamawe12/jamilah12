2257c26d91e4c7bd932c8cc2d895f11993d302ae
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-04 08:52:52 +0700

    Update blogwaadst1dr3red.js

--- blogwaadst1dr3red.js
+++ blogwaadst1dr3red.js
@@ -1,7 +1,7 @@
 var dataUrl = [
   
   'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-1.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/05/grup-2.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/05/grup-2_31.html',
   'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-3.html',
   'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-4.html',
   'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-5_2.html',
