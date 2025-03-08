77bf4723c47b293e5e5246300d597425eb1a954d
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-08 16:00:27 +0700

    Update blogwaadst1dr3redbaru.js

--- blogwaadst1dr3redbaru.js
+++ blogwaadst1dr3redbaru.js
@@ -1,13 +1,14 @@
 var dataUrl = [
   
-  'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-1.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/05/grup-2_31.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-3.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-4.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/03/grupp-5.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/03/grupp-4.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/03/grupp-3.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/03/grupp-2.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/03/grupp-1.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-10.html',
   'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-6.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-7_3.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-8_3.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-10.html'
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-3.html',
+  'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-1.html'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
