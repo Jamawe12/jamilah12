2f5b071c382a3c3d4b636be17e3ce79dc8ce5ea9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-08 11:56:58 +0700

    Create blogwaadst1dr3redbaru.js

---
+++ blogwaadst1dr3redbaru.js
@@ -0,0 +1,13 @@
+var dataUrl = [
+  
+  'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-1.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/05/grup-2_31.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-3.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-4.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-6.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-7_3.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-8_3.html',
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-10.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
