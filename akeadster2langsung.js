9c53150d03315256a5ee0a611aa81fa908a562c1
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-11 13:06:33 +0700

    Create akeadster2langsung.js

---
+++ akeadster2langsung.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+  'https://turflivestockdetector.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
+  'https://turflivestockdetector.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
+  'https://turflivestockdetector.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
