1066d84982b72bc6dd961c9ccc6a4e82a67dc29d
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-14 23:30:07 +0700

    Update blogwadr3ads2.js

--- blogwadr3ads2.js
+++ blogwadr3ads2.js
@@ -1,8 +1,1 @@
-var dataUrl = [
-  
-  'https://slickquiver.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
-  'https://slickquiver.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
-  'https://slickquiver.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
+
