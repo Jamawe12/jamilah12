da4c1fc44c74d20e047a3f0b610e0dfb8257fe15
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-14 23:12:25 +0700

    Update ads2dr3blog2.js

--- ads2dr3blog2.js
+++ ads2dr3blog2.js
@@ -1,16 +1,1 @@
-var dataUrl = [
 
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-1.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-2.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-3.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-4.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-5.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-6.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-7.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-8.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-9.html',
-  'https://berbagihadiah24.blogspot.com/2024/11/berbagi-hadiah-10.html'
-  
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
