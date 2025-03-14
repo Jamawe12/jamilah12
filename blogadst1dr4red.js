153f13a2b2fb090328edfce8b99dc2be5b99a4a5
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-14 11:09:06 +0700

    Create blogadst1dr4red.js

---
+++ blogadst1dr4red.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-1.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-2.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-3.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-4.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-5.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-6.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-7.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-8.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-9.html',
+  'https://fullplayvideoo.blogspot.com/2025/03/grup-10.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
