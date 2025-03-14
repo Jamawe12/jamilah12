a4def097bd64f7bc40ac59c9a53ebddaa8bdb06c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-14 10:52:04 +0700

    Update blogads1dr4apk.js

--- blogads1dr4apk.js
+++ blogads1dr4apk.js
@@ -1,10 +1,1 @@
-var dataUrl = [
-  
-  'https://fullaplikasivideo.blogspot.com/p/video-viral-1.html',
-  'https://fullaplikasivideo.blogspot.com/p/video-viral-2.html',
-  'https://fullaplikasivideo.blogspot.com/p/video-viral-3.html',
-  'https://fullaplikasivideo.blogspot.com/p/video-viral-4.html',
-  'https://fullaplikasivideo.blogspot.com/p/video-viral-5.html'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
+
