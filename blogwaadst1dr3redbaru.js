6a12d4911ccff1c6cf9902a333b8d7cecac77a92
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-03-20 23:19:06 +0700

    Update blogwaadst1dr3redbaru.js

--- blogwaadst1dr3redbaru.js
+++ blogwaadst1dr3redbaru.js
@@ -4,8 +4,7 @@ var dataUrl = [
   'https://chattgrupwhatsappp.blogspot.com/2025/03/grupp-2.html',
   'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-10.html',
   'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-6.html',
-  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-3.html',
-  'https://chattgrupwhatsappp.blogspot.com/2025/02/grup-1.html'
+  'https://chattgrupwhatsappp.blogspot.com/2024/06/grup-3.html'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
