7f90f08f83af7b4750249d7c7004eb7989a4b159
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-04-09 16:32:06 +0700

    Delete contohblog.js

--- contohblog.js
+++
@@ -1,8 +0,0 @@
-var dataUrl = [
-
-  'https://blogutamared.blogspot.com/2024/12/post-1.html',
-  'https://blogutamared.blogspot.com/2024/12/post-1.html'
-  
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
