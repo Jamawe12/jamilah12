33a1826731b95f5c4c5c2f705834c918b3c053a9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-05-01 14:46:34 +0700

    Update blogadster1dr3red.js

--- blogadster1dr3red.js
+++ blogadster1dr3red.js
@@ -1,12 +1,10 @@
 var dataUrl = [
   
-  'https://chattwhatsapvideo.blogspot.com/2025/03/grup-1.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-2.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-3.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-4.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-5.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-6.html',
-  'https://chattwhatsapvideo.blogspot.com/2025/03/grup-7.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-8.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-9.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-10.html'
