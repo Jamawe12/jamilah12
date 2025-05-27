9c8377433f0b02606247795f6d06f0728b25b017
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-05-27 16:08:49 +0700

    Create blogadsensered.js

---
+++ blogadsensered.js
@@ -0,0 +1,15 @@
+var dataUrl = [
+  
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-1.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-2.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-3.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-4.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-5.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-6.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-7.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-8.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-9.html',
+  'https://chattgrupwaafull.blogspot.com/2025/05/grup-wa-10.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
