e325ee40dca47c13c3799a113f5b669aef74b0d5
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-05-30 22:29:00 +0700

    Update blogadster1dr3red.js

--- blogadster1dr3red.js
+++ blogadster1dr3red.js
@@ -4,9 +4,7 @@ var dataUrl = [
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-3.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-4.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-5.html',
-  'https://chattwhatsapvideo.blogspot.com/2025/03/grup-6.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-8.html',
-  'https://chattwhatsapvideo.blogspot.com/2025/03/grup-9.html',
   'https://chattwhatsapvideo.blogspot.com/2025/03/grup-10.html'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
