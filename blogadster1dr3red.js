585e272848b9dea48ce8cfda68ac27f11c225d46
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-06-09 09:30:18 +0700

    Update blogadster1dr3red.js

--- blogadster1dr3red.js
+++ blogadster1dr3red.js
@@ -1,15 +1,5 @@
 var dataUrl = [
   
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-10.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-9.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-8.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-7.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-6.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-5.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-4.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-3.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-2.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/05/grupp-1.html",
   "https://chattwhatsapvideo.blogspot.com/2025/05/grupp-10.html",
   "https://chattwhatsapvideo.blogspot.com/2025/05/grupp-9.html",
   "https://chattwhatsapvideo.blogspot.com/2025/05/grupp-8.html",
@@ -20,11 +10,19 @@ var dataUrl = [
   "https://chattwhatsapvideo.blogspot.com/2025/05/grupp-3.html",
   "https://chattwhatsapvideo.blogspot.com/2025/05/grupp-2.html",
   "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-1.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-10.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-9.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-8.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-7.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-6.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-5.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-4.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-3.html",
+  "https://chattwhatsapvideo.blogspot.com/2025/06/gruup-2.html",
   "https://chattwhatsapvideo.blogspot.com/2025/03/grup-10.html",
   "https://chattwhatsapvideo.blogspot.com/2025/03/grup-8.html",
   "https://chattwhatsapvideo.blogspot.com/2025/03/grup-5.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/03/grup-3.html",
-  "https://chattwhatsapvideo.blogspot.com/2025/03/grup-2.html"
+  "https://chattwhatsapvideo.blogspot.com/2025/03/grup-3.html"
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
