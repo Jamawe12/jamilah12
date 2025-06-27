43633b108c945afd73fcd34910b0e3fdf43f5dcd
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-06-28 01:30:35 +0700

    Create akeadster2dr1langsung.js

---
+++ akeadster2dr1langsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://grievedvicerecord.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
+
+  'https://grievedvicerecord.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
+
+  'https://grievedvicerecord.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
