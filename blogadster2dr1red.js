086523c3b95d907da95e18dae26b70440d253e01
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-06-28 11:29:17 +0700

    Create blogadster2dr1red.js

---
+++ blogadster2dr1red.js
@@ -0,0 +1,25 @@
+var dataUrl = [
+  
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-20.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-19.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-18.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-17.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-16.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-15.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-14.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-13.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-12.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-11.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-10.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-9.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-8.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-7.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-6.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-5.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-4.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-3.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-2.html",
+  "https://albumvideooasikin.blogspot.com/2025/06/play-video-1.html"
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
