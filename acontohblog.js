6f5c24ff96b823ed0f520a4e43ec3e6ede9e58cf
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-01 13:02:05 +0700

    Create acontohblog.js

---
+++ acontohblog.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://playyvideoonyadisini.blogspot.com/2025/05/post-4.html',
+
+  'https://playyvideoonyadisini.blogspot.com/2025/05/post-4.html',
+
+  'https://playyvideoonyadisini.blogspot.com/2025/05/post-4.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
