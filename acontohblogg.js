930ab9e97012692627e2141f9e96c5793471200e
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-02 17:13:33 +0700

    Create acontohblogg.js

---
+++ acontohblogg.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://playvideooterbaruu1.blogspot.com/2025/05/play-video_39.html',
+
+  'https://playvideooterbaruu1.blogspot.com/2025/05/play-video_39.html',
+
+  'https://playvideooterbaruu1.blogspot.com/2025/05/play-video_39.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
