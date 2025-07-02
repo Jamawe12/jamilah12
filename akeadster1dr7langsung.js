c5b44bb04764a490dfd774e36445b647ab564c10
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-02 15:55:04 +0700

    Create akeadster1dr7langsung.js

---
+++ akeadster1dr7langsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://contestjoe.com/rgvpir7j?key=7b7c8d1810a533f6d501cc194bf03d4f',
+
+  'https://contestjoe.com/rgvpir7j?key=7b7c8d1810a533f6d501cc194bf03d4f',
+
+  'https://contestjoe.com/rgvpir7j?key=7b7c8d1810a533f6d501cc194bf03d4f'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
