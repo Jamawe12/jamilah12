bdeeb15884c1dc36b554cab3ae17b4343cbc7126
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-02 23:46:12 +0700

    Create blogadster1dr8red.js

---
+++ blogadster1dr8red.js
@@ -0,0 +1,19 @@
+var dataUrl = [
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-1.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-2.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-3.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-4.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-5.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-6.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-7.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-8.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-9.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-10.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-11.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-12.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-13.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-14.html',
+  'https://playyfullvideo.blogspot.com/2025/06/play-video-15.html'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
