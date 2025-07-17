6520b2d841af366233e2e8ee788cfe1f3e0c842b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-17 10:17:27 +0700

    Create zblogshare1threads.js

---
+++ zblogshare1threads.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://playplayvideo1.blogspot.com/',
+'https://playplayvideo2.blogspot.com/',
+'https://playplayvideo3.blogspot.com/',
+'https://playplayvideo4.blogspot.com/',
+'https://playplayvideo5.blogspot.com/',
+'https://playplayvideo6.blogspot.com/',
+'https://playplayvideo7.blogspot.com/',
+'https://playplayvideo8.blogspot.com/',
+'https://playplayvideo9.blogspot.com/',
+'https://playplayvideo10.blogspot.com/',
+'https://playplayvideo11.blogspot.com/',
+'https://playplayvideo12.blogspot.com/',
+'https://playplayvideo13.blogspot.com/',
+'https://playplayvideo14.blogspot.com/',
+'https://playplayvideo15.blogspot.com/',
+'https://playplayvideo16.blogspot.com/',
+'https://playplayvideo17.blogspot.com/',
+'https://playplayvideo18.blogspot.com/',
+'https://playplayvideo19.blogspot.com/',
+'https://playplayvideo20.blogspot.com/'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
