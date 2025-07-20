34c09772b9733cfdf3d11f3379733e8f2e0b1196
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-07-20 22:47:27 +0700

    Update akeadster1dr3langsung.js

--- akeadster1dr3langsung.js
+++ akeadster1dr3langsung.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+  'https://cellspitch.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
 
-  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+  'https://cellspitch.com/x9wigvew?key=578a093a19f2f6271ad8cd37f21128777',
 
-  'https://contestjoe.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
+  'https://cellspitch.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
