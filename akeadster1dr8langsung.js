57d9ed5cad8c7f497dde57becb684ecf536f1ae2
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-01 15:43:23 +0700

    Update akeadster1dr8langsung.js

--- akeadster1dr8langsung.js
+++ akeadster1dr8langsung.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'https://contestjoe.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc',
+  'https://ignoblerectifymargaret.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc',
 
-  'https://contestjoe.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc',
+  'https://ignoblerectifymargaret.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc',
 
-  'https://contestjoe.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc'
+  'https://ignoblerectifymargaret.com/z2a928v4?key=ba5d7447db530b3357389c4c38ec6abc'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
