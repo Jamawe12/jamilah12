e584449a06a0eccb41057bc0c2b951592e03c3fc
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-04 15:30:40 +0700

    Update akeadster1dr3langsung.js

--- akeadster1dr3langsung.js
+++ akeadster1dr3langsung.js
@@ -1,10 +1,10 @@
 var dataUrl = [
 
-  'https://ignoblerectifymargaret.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+  'https://embarrasspiled.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
 
-  'https://ignoblerectifymargaret.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
+  'https://embarrasspiled.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877',
 
-  'https://ignoblerectifymargaret.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
+  'https://embarrasspiled.com/x9wigvew?key=578a093a19f2f6271ad8cd37f2112877'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
