f4492eb4c03761090358841b888a2ebe8015b486
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-05 23:05:24 +0700

    Update ads1threadss1.js

--- ads1threadss1.js
+++ ads1threadss1.js
@@ -1,24 +1,22 @@
 var dataUrl = [
 
-  'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT0AJgx_SKR05y43VY88uRXjB39zuXhsyZ5w1G2vvYsQ8-A3E3tnkXj0QXidm9a8uAm_ul8mE3dq_6sgbg8R1FIrB6aPA6DNc8SKbvtdJLhuyNtS3nBJBq457OmBCpBy2b4Wd2w',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3DXZXDR4T&e=AT1Tz8zaAvXl6OiVj9cnmvVTh0s7Ts1RsJlTO9EbyDXxZdOGbnvH0BpvTz69bv0a_X2tODEF0iPYKWmiQOtjXBc3fS-eDpJWobxjpj_n9asBzKUgMKdZaHMSYoni53RmPSNstjs',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT1-xmv-vRBw5rYCyxbgYLu-2uSigSlVf6elWRTnEjHjMKwnOpEp8c3i1l7FA7cHIzXRampKGu3LjR7YGJRa4nZioBrOqX4aYWzCRV_MPnUxfl71KdvNGF1k_o8opPKWnAtO3uM',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT1Tz8zaAvXl6OiVj9cnmvVTh0s7Ts1RsJlTO9EbyDXxZdOGbnvH0BpvTz69bv0a_X2tODEF0iPYKWmiQOtjXBc3fS-eDpJWobxjpj_n9asBzKUgMKdZaHMSYoni53RmPSNstjs',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT3lTFGFV2KoHDevfO20Ng63ifw29T7S_tlwOKko4U4tWbcKxjCnPbrFcxJct-CBUakscojmDdLlS-72e7EE5cwnc2i6FwI0xZK-ogY5TrMDLYnF-RG7_g47waiPIcOf1R2Dxqc',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT1Tz8zaAvXl6OiVj9cnmvVTh0s7Ts1RsJlTO9EbyDXxZdOGbnvH0BpvTz69bv0a_X2tODEF0iPYKWmiQOtjXBc3fS-eDpJWobxjpj_n9asBzKUgMKdZaHMSYoni53RmPSNstjs',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT02Iyj8DSxteSIBzUWxM_qyx4qCm9FTRwuJYeIHH1C8nduAo93d-QfHS1WjQtoB465bSs_cCmpCXjh10sDIkkDkZuE4CMEnPPUO7tZBdiIHITJuFUq1_naUlffkTJlLtKjVd3E',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3DXZXDR4T&e=AT0nq0qAx-jKB5QiDS3nej-Kwyy4MMTOW8CNUVS-v5x4tNck1kYEOQs1SBEBUp8HL0qvjVWjqS8J8NxC80s50jOn13lhVfQ5ondYbI9iHz2D8h3BT9k8PPxVsOCQHPVIpwscI2E',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT305qSoLWFND_x4tFNxDd7MFrv-FpMEtv4wA1F8uK6dBgoFxo2XFssmuR9nIxr7zTVTQcngQJPWwcdeXrj_8c2hsQp_hfHQG_ZJ7XKdBk0bOOh-_Z47kDcvdlRiOkIePVp4w1Y',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT0nq0qAx-jKB5QiDS3nej-Kwyy4MMTOW8CNUVS-v5x4tNck1kYEOQs1SBEBUp8HL0qvjVWjqS8J8NxC80s50jOn13lhVfQ5ondYbI9iHz2D8h3BT9k8PPxVsOCQHPVIpwscI2E',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT06mGqZVlleweTQNzrKnfw7RRm339K_5-nuPDlw1qB--UAURoVSoVXzPnv2RxVhcu5TtHMmSKDUM66GSsAzhPsiCTPhhuq_9Q-aHuhbNZpsbdcehGOKLZ33_AzQpo6ZgNavkaQ',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT0nq0qAx-jKB5QiDS3nej-Kwyy4MMTOW8CNUVS-v5x4tNck1kYEOQs1SBEBUp8HL0qvjVWjqS8J8NxC80s50jOn13lhVfQ5ondYbI9iHz2D8h3BT9k8PPxVsOCQHPVIpwscI2E',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT1TYjZ4b1e_AIxqZYP9BL8DnoDrtdBGxgB6kfdPQnFDWYpzMzchTTM5lSK1qFBnlKJ20TVDTKwe2kq4Q9uGzrp4xt-RZxi03gDmE_XV97VaCDsGinnrcctvCRSC8N2FXZAIXk0',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3DXZXDR4T&e=AT10edwxhPi9UPtFMCvxtl0eg_J9uxUnZPwI5EctvTw1d4Grcm284pHBOScZj73IPCmZm1QEtqVe2XcNhrT8itc8fy7A9xFEOrdRWPoFhMYny6Vyo_6UX0Zrk5txDGUJUy7XXic',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT2N78T0RyZhJYKzB5oCc0uOlwsEv0baYK_J5utZ9qZ3Tx_i4m7aR4BtzN2lU-S6PNWJY1TOZBofAk9fljWGQu4IqN5jEjzRPSMxqviP59aAf-XWsQjAe9Bb_6uY1LW8ErM3mog',
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT10edwxhPi9UPtFMCvxtl0eg_J9uxUnZPwI5EctvTw1d4Grcm284pHBOScZj73IPCmZm1QEtqVe2XcNhrT8itc8fy7A9xFEOrdRWPoFhMYny6Vyo_6UX0Zrk5txDGUJUy7XXic',
 
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT1X2nShXIQIq0ViqCLmecbfb32cAiu9szWPrFsfQaJM2omCuYbbAFp6lGx9z77jFcZU-rHXSJxDW0icpRkaq-8HtoI20gebwz2_ctKf_HFTKVoOpEytukbg5lACKaDA8p2D8L0',
-
-'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT1cLvyCVlyrdi3PmcO5apCKWpso6o5NKkqGeFWCCPzItolEGQURA6RFgcX-fiB7Lol8CbHD382OGU3u2XAbDv9EhxdjmK07SttGuHMZROGKRK8KzIguK_bt5QmPuq46Sjig9jI'
+'https://l.threads.com/?u=https%3A%2F%2Fvidcy.de%2Fv%2F%3Fid%3D42QDSW7&e=AT10edwxhPi9UPtFMCvxtl0eg_J9uxUnZPwI5EctvTw1d4Grcm284pHBOScZj73IPCmZm1QEtqVe2XcNhrT8itc8fy7A9xFEOrdRWPoFhMYny6Vyo_6UX0Zrk5txDGUJUy7XXic'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
