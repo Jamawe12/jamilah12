f363284961292c7509529b3606ed26ff57638af6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-07 11:39:44 +0700

    Create ablodadselangsung.js

---
+++ ablodadselangsung.js
@@ -0,0 +1,27 @@
+var dataUrl = [
+
+'https://waaae.me/bad8c1',
+'https://chattwa.me/d3befb',
+'https://chtwa.me/90feca',
+'https://terabox.ltd/4jYHYdIWg8GoTvTS45Hh38',
+'https://terabox.ltd/FToyUXKKwSxBWl7JgdO7ah',
+'https://terabox.ltd/vkI0kJ1gfnPcGoPn4aHFpR',
+'https://terabox.ltd/a880cb',
+'https://terabox.ltd/bbL870ek403DnIuHvV8S8p',
+'https://terabox.ltd/0NsJIYjjRvXj62UbqpmuKv',
+'https://terabox.ltd/8Ew3sze1DeKR441gJdju4n',
+'https://vidcy.de/quNIZ6GcMyNZwmeRqTJtmd',
+'https://vidcy.de/v/?id=XZXDR4T',
+'https://vidcy.de/16roH3DqgtGmkckvuEaR7w',
+'https://terabox.ltd/bc6zkqR7I0sZNKMOQ18COR',
+'https://waaae.me/28b742',
+'https://waaae.me/a40801',
+'https://chattwa.me/84e259',
+'https://chattwa.me/a1cf87',
+'https://chattwa.me/f7fdc7',
+'https://chattwa.me/ae03e7',
+'https://vidcy.de/v/?id=42QDSW7'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
