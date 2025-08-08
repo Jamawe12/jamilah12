490784819ecd620bba00f597559c0fac18cdf5a5
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-08 10:49:48 +0700

    Create blogadsenred1.js

---
+++ blogadsenred1.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://www.videyw.my.id/2025/08/play-20.html',
+'https://www.videyw.my.id/2025/08/play-19.html',
+'https://www.videyw.my.id/2025/08/play-18.html',
+'https://www.videyw.my.id/2025/08/play-17.html',
+'https://www.videyw.my.id/2025/08/play-16.html',
+'https://www.videyw.my.id/2025/08/play-15.html',
+'https://www.videyw.my.id/2025/08/play-14.html',
+'https://www.videyw.my.id/2025/08/play-13.html',
+'https://www.videyw.my.id/2025/08/play-12.html',
+'https://www.videyw.my.id/2025/08/play-11.html',
+'https://www.videyw.my.id/2025/08/play-10.html',
+'https://www.videyw.my.id/2025/08/play-9.html',
+'https://www.videyw.my.id/2025/08/play-8.html',
+'https://www.videyw.my.id/2025/08/play-7.html',
+'https://www.videyw.my.id/2025/08/play-6.html',
+'https://www.videyw.my.id/2025/08/play-5.html',
+'https://www.videyw.my.id/2025/08/play-4.html',
+'https://www.videyw.my.id/2025/08/play-3.html',
+'https://www.videyw.my.id/2025/08/play-2.html',
+'https://www.videyw.my.id/2025/08/play-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
