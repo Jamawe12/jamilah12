fc1e3e9616b02a76ad980b10edcf2a139ca71a21
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-08 10:38:20 +0700

    Create ablogadsenlangsungg.js

---
+++ ablogadsenlangsungg.js
@@ -0,0 +1,28 @@
+var dataUrl = [
+
+'https://vidcy.de/v/?id=WG6IUHY',
+'https://vidcy.de/v/?id=3RHKT1K',
+'https://vidcy.de/v/?id=Q5OUM0Z',
+'https://vidcy.de/v/?id=WQWJ172',
+'https://vidcy.de/v/?id=21RA8GH',
+'https://vidcy.de/v/?id=HM7ZK4G',
+'https://vidcy.de/v/?id=G1KLQKX',
+'https://vidcy.de/v/?id=O1ZV5TD',
+'https://vidcy.de/v/?id=XZXDR4T',
+'https://vidcy.de/v/?id=42QDSW7',
+'https://waaae.me/bad8c1',
+'https://chattwa.me/d3befb',
+'https://chtwa.me/90feca',
+'https://waaae.me/28b742',
+'https://waaae.me/a40801',
+'https://terabox.ltd/qyW55QuF391NY8picN0MXx',
+'https://terabox.ltd/4jYHYdIWg8GoTvTS45Hh38',
+'https://terabox.ltd/FToyUXKKwSxBWl7JgdO7ah',
+'https://terabox.ltd/vkI0kJ1gfnPcGoPn4aHFpR',
+'https://terabox.ltd/bbL870ek403DnIuHvV8S8p',
+'https://terabox.ltd/0NsJIYjjRvXj62UbqpmuKv',
+'https://terabox.ltd/8Ew3sze1DeKR441gJdju4n'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
