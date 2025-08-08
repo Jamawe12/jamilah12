e131ef13d0232ac166d896832d1b22d9f379d320
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-08 12:24:52 +0700

    Create ablogadsenlangsungg2.js

---
+++ ablogadsenlangsungg2.js
@@ -0,0 +1,31 @@
+var dataUrl = [
+
+'https://vidcy.de/v/?id=WG6IUHY',
+'https://vidcy.de/v/?id=3RHKT1K',
+'https://vidcy.de/v/?id=Q5OUM0Z',
+'https://vidcy.de/v/?id=WQWJ172',
+'https://vidcy.de/v/?id=21RA8GH',
+'https://vidcy.de/v/?id=HM7ZK4G',
+'https://vidcy.de/v/?id=G1KLQKX',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
