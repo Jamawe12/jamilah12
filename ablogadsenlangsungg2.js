8ac43691a4d7381b5b871ab79d7917ee6616f305
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-18 11:44:06 +0700

    Update ablogadsenlangsungg2.js

--- ablogadsenlangsungg2.js
+++ ablogadsenlangsungg2.js
@@ -1,12 +1,9 @@
 var dataUrl = [
 
-'https://vidcy.de/v/?id=WG6IUHY',
-'https://vidcy.de/v/?id=3RHKT1K',
-'https://vidcy.de/v/?id=Q5OUM0Z',
-'https://vidcy.de/v/?id=WQWJ172',
-'https://vidcy.de/v/?id=21RA8GH',
-'https://vidcy.de/v/?id=HM7ZK4G',
-'https://vidcy.de/v/?id=G1KLQKX',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
 'https://billimiters.ayoah.com/videy',
 'https://billimiters.ayoah.com/videy',
 'https://billimiters.ayoah.com/videy',
