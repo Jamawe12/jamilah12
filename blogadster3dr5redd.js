f12f97b66a72144aea8fe70e5cb2e10fcf5671b9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-18 13:27:43 +0700

    Create blogadster3dr5redd.js

---
+++ blogadster3dr5redd.js
@@ -0,0 +1,36 @@
+var dataUrl = [
+  
+'https://videyvideocoo.blogspot.com/2025/08/play-video-30.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-29.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-28.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-27.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-26.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-25.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-24.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-23.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-22.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-21.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-20.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-19.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-18.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-17.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-16.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-15.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-14.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-13.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-12.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-11.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-10.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-9.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-8.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-7.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-6.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-5.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-4.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-3.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-2.html',
+'https://videyvideocoo.blogspot.com/2025/08/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
