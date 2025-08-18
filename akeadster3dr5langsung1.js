5a76e319488f79270e9c1fbecfc56089ecdc9c04
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-08-18 12:28:58 +0700

    Create akeadster3dr5langsung1.js

---
+++ akeadster3dr5langsung1.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://excitedgleamingrefrigerate.com/bmkhx5f6?key=8fc0d28bfc53f85e46eb4e3e623d9630',
+
+  'https://excitedgleamingrefrigerate.com/bmkhx5f6?key=8fc0d28bfc53f85e46eb4e3e623d9630',
+
+  'https://excitedgleamingrefrigerate.com/bmkhx5f6?key=8fc0d28bfc53f85e46eb4e3e623d9630'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
