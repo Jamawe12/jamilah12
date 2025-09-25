455cd034992c5cfc2a577b2eefa198afa849abc3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-09-25 22:23:06 +0700

    Delete ablogadsenlangsungg.js

--- ablogadsenlangsungg.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://jarthreads.brandballia.com/threads',
-'https://jarthreads.brandballia.com/threads',
-'https://jarthreads.brandballia.com/threads'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
