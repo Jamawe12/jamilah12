7dbfa14a4e73fecdd11507353f1328b621c6bc04
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-09-25 14:56:20 +0700

    Update ablogadsenlangsungg2.js

--- ablogadsenlangsungg2.js
+++ ablogadsenlangsungg2.js
@@ -1,27 +1,8 @@
 var dataUrl = [
 
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/videy',
-'https://billimiters.ayoah.com/whatsapp',
-'https://billimiters.ayoah.com/whatsapp',
-'https://billimiters.ayoah.com/whatsapp',
-'https://billimiters.ayoah.com/whatsapp',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating',
-'https://billimiters.ayoah.com/dating'
+'https://jarthreads.brandballia.com/threads',
+'https://jarthreads.brandballia.com/threads',
+'https://jarthreads.brandballia.com/threads'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
