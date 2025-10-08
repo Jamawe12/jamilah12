37dad6cbf5ed1d5b3a0a08c873b323b30d56c2b6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-10-08 10:11:30 +0700

    Create akeadsterminnggulangsung.js

---
+++ akeadsterminnggulangsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://cratestable.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+
+  'https://cratestable.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+
+  'https://cratestable.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
