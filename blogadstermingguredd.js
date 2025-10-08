1f1e29ae35e01c3408eba5b27df72594e8e6ded8
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-10-08 20:19:16 +0700

    Update blogadstermingguredd.js

--- blogadstermingguredd.js
+++ blogadstermingguredd.js
@@ -1,5 +1,12 @@
 var dataUrl = [
 
+'https://playvideodood.blogspot.com/2025/10/play-video-20.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-19.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-18.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-17.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-16.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-15.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-14.html',
 'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-20.html',
 'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-19.html',
 'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-18.html',
@@ -19,7 +26,27 @@ var dataUrl = [
 'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-4.html',
 'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-3.html',
 'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-2.html',
-'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-1.html'
+'https://kumpulanvideoofull.blogspot.com/2025/10/play-video-1.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-20.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-19.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-18.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-17.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-16.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-15.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-14.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-13.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-12.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-11.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-10.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-9.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-8.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-7.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-6.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-5.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-4.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-3.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-2.html',
+'https://videohdddfull.blogspot.com/2025/10/play-video-1.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
