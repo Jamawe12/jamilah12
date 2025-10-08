4c3fa541466df934104f437e4c30bbcbbf3fb7a9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-10-08 19:54:33 +0700

    Create akeadsterminnggu2langsung.js

---
+++ akeadsterminnggu2langsung.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
