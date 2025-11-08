0025f2642b0787f3de8da9228d9f0850f51b142c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-11-08 12:10:37 +0700

    Create blogredkeplayyviideo.js

---
+++ blogredkeplayyviideo.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-30.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-29.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-28.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-27.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-26.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-25.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-24.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-23.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-22.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-21.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-20.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-19.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-18.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-17.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-16.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-15.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-14.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-13.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-12.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-11.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
