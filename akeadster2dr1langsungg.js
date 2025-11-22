a5cd885680eda6da20f532290114115f384abdb0
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-11-22 17:23:20 +0700

    Create akeadster2dr1langsungg.js

---
+++ akeadster2dr1langsungg.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://www.effectivegatecpm.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
+
+  'https://www.effectivegatecpm.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6',
+
+  'https://www.effectivegatecpm.com/i2g8say93?key=f1c5d44d51a9aeeb99d202c481a619c6'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
