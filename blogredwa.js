ecc221a911e7cd2a954dcc418c4b07e71cb59be6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-11-24 13:29:09 +0700

    Create blogredwa.js

---
+++ blogredwa.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://chattwhatsappp.blogspot.com/2025/11/chatt-whatsapp-5.html',
+'https://chattwhatsappp.blogspot.com/2025/11/chatt-whatsapp-4.html',
+'https://chattwhatsappp.blogspot.com/2025/11/chatt-whatsapp-3.html',
+'https://chattwhatsappp.blogspot.com/2025/11/chatt-whatsapp-2.html',
+'https://chattwhatsappp.blogspot.com/2025/11/chatt-whatsapp-1.html',
+'https://chattwhatsappgruppfull.blogspot.com/2025/11/chatt-whatsapp-5.html',
+'https://chattwhatsappgruppfull.blogspot.com/2025/11/chatt-whatsapp-4.html',
+'https://chattwhatsappgruppfull.blogspot.com/2025/11/chatt-whatsapp-3.html',
+'https://chattwhatsappgruppfull.blogspot.com/2025/11/chatt-whatsapp-2.html',
+'https://chattwhatsappgruppfull.blogspot.com/2025/11/chatt-whatsapp-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
