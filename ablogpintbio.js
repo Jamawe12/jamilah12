3696b4bfd888839e2b376876c978717faf185bc7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-12-11 23:42:34 +0700

    Create ablogpintbio.js

---
+++ ablogpintbio.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-20.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-19.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-18.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-17.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-16.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-15.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-14.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-13.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-12.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-11.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-10.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-9.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-8.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-7.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-6.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-5.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-4.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-3.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-2.html',
+'https://fulllplayyvideoo.blogspot.com/2025/12/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
