1046eacd9cedb2bc7bee359470e094e828b73fec
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-12-11 23:36:15 +0700

    Create ablogpintwabio.js

---
+++ ablogpintwabio.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-20.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-19.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-18.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-17.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-16.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-15.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-14.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-13.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-12.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-11.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-10.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-9.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-8.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-7.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-6.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-5.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-4.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-3.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-2.html',
+'https://chattwhaatsappgrupp.blogspot.com/2025/12/grup-wa-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
