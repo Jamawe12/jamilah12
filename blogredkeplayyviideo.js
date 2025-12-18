6536825bdfc0248d48d0508cba5e8c996bc8a2a1
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-12-18 09:10:21 +0700

    Update blogredkeplayyviideo.js

--- blogredkeplayyviideo.js
+++ blogredkeplayyviideo.js
@@ -3,7 +3,6 @@ var dataUrl = [
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-30.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-29.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-28.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-27.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-26.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-25.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-24.html',
@@ -18,7 +17,6 @@ var dataUrl = [
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-15.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-14.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-13.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-12.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-11.html'
 
 ];
