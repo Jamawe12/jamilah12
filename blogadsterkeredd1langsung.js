419e1c62cc0e7fef245e599d6972ba645a557148
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-12-18 09:12:37 +0700

    Update blogadsterkeredd1langsung.js

--- blogadsterkeredd1langsung.js
+++ blogadsterkeredd1langsung.js
@@ -3,12 +3,11 @@ var dataUrl = [
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-30.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-29.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-28.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-27.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-26.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-25.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-24.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-22.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-23.html',
+'https://playyviideooindoo.blogspot.com/2025/10/play-video-22.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-21.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-20.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-19.html',
@@ -18,7 +17,6 @@ var dataUrl = [
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-15.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-14.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-13.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-12.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-11.html'
 
 ];
