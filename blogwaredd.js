8ebe079346a112ec3cd7eb3c6f92e65851882b26
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2025-12-26 16:17:15 +0700

    Create blogwaredd.js

---
+++ blogwaredd.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-20.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-19.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-18.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-17.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-16.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-15.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-14.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-1.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-13.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-6.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-12.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-11.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-10.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-9.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-8.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-7.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-5.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-4.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-3.html',
+'https://chattwhaatsapp.blogspot.com/2025/12/grup-wa-2.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
