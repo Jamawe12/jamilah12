a65096560e940f36cfbe76dc3290bbc343d62513
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-01-08 10:42:01 +0700

    Update bloggrupwared.js

--- bloggrupwared.js
+++ bloggrupwared.js
@@ -1,35 +1,23 @@
 var dataUrl = [
 
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-45.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-43.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-41.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-40.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-39.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-38.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-37.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-36.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-34.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-33.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-31.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-30.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-29.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-27.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-26.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-25.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-23.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-22.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-16.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-15.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-14.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-13.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-12.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-10.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-8.html',
 'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-7.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-6.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-4.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-3.html',
-'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-1.html'
+'https://chattgruppwhatsaapp.blogspot.com/2025/12/play-video-3.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
