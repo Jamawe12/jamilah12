48ac37a39cb4450736c58d454e84c26fd54676e3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-01-09 11:19:00 +0700

    Create blogadsttokoo.js

---
+++ blogadsttokoo.js
@@ -0,0 +1,36 @@
+var dataUrl = [
+
+'https://vt.tokopedia.com/t/ZSHEGmcDssRJT-pYx1z',
+'https://vt.tokopedia.com/t/ZSHEGmwkdMGwW-4iapm',
+'https://vt.tokopedia.com/t/ZSHEGmERunMNX-82s3l',
+'https://vt.tokopedia.com/t/ZSHEGmo1t4tT7-fLism',
+'https://vt.tokopedia.com/t/ZSHEGuJ9SLAvj-JPzMV',
+'https://vt.tokopedia.com/t/ZSHEGuR5pJvyh-8e7hA',
+'https://vt.tokopedia.com/t/ZSHEGuR4wdWqU-GP6pP',
+'https://vt.tokopedia.com/t/ZSHEGuNXaEvbf-xldPN',
+'https://vt.tokopedia.com/t/ZSHEGujcfG495-MNoEc',
+'https://vt.tokopedia.com/t/ZSHEGuM2mQbWq-r0ptQ',
+'https://vt.tokopedia.com/t/ZSHEGuygFnD93-L8zrS',
+'https://vt.tokopedia.com/t/ZSHEGuPoGbWRt-m01Ts',
+'https://vt.tokopedia.com/t/ZSHEGuxRv1jyg-pHf8D',
+'https://vt.tokopedia.com/t/ZSHEGuXx8b2dB-MuTTp',
+'https://vt.tokopedia.com/t/ZSHEGu4p8hR57-yPYoF',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c',
+'https://cratestable.com/vi2ihsmjf2?key=51ff2b490dde92265ca1fa26874f925c'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
