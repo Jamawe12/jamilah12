1f676d0b12eafad70ff07bde3b669853a91efcdf
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-01-14 12:21:47 +0700

    Create blogvkredd.js

---
+++ blogvkredd.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-20.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-19.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-18.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-17.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-16.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-15.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-14.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-13.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-12.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-11.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-10.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-9.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-8.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-7.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-6.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-5.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-4.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-3.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-2.html',
+'https://myplayyvideoofull.blogspot.com/2026/01/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
