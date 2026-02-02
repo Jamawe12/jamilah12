d8704673f6c1e1976cb71d07028dad2b08650ecc
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-02 11:06:43 +0700

    Create akemonetagtwittbaru.js

---
+++ akemonetagtwittbaru.js
@@ -0,0 +1,22 @@
+var dataUrl = [
+
+'https://t.co/mNDOlRbZdx',
+'https://t.co/tSGqG9Kr9f',
+'https://t.co/tN5eI7NIuI',
+'https://t.co/HAcRkZeqgd',
+'https://t.co/ay7yKS8T3M',
+'https://t.co/z6eS1ma7x3',
+'https://t.co/PEYxFxkmWQ',
+'https://t.co/Y2CH0RraZu',
+'https://t.co/uolf3QWkN2',
+'https://t.co/xgqMVYyRR0',
+'https://t.co/TuO6wKemni',
+'https://t.co/CX50s4Zrn2',
+'https://t.co/GuQhaYqJb0',
+'https://t.co/RN47i1aAba',
+'https://t.co/td4UExlQAv',
+'https://t.co/f95dGNm4Kb'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
