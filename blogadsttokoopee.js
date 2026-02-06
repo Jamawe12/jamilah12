e66f0b655c4e0604c397a21e4273acb6e37eba51
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-06 21:05:40 +0700

    Create blogadsttokoopee.js

---
+++ blogadsttokoopee.js
@@ -0,0 +1,36 @@
+var dataUrl = [
+
+'https://vt.tokopedia.com/t/ZS9JddDto48dq-TS2kp/',
+'https://vt.tokopedia.com/t/ZS9JdduJgRL3Q-yEpSs/',
+'https://vt.tokopedia.com/t/ZS9JddxDrEk6A-QzSaK/',
+'https://vt.tokopedia.com/t/ZS9Jdd9tHoebY-feXmE/',
+'https://vt.tokopedia.com/t/ZS9JddnPuJvbj-kJtnm/',
+'https://vt.tokopedia.com/t/ZS9Jdd7c2XHQk-bYVXL/',
+'https://vt.tokopedia.com/t/ZS9Jdd3D3Djw3-ZBraP/',
+'https://vt.tokopedia.com/t/ZS9Jddog96gNe-1Wg2U/',
+'https://vt.tokopedia.com/t/ZS9JdRc7oDW9E-4dSuL/',
+'https://vt.tokopedia.com/t/ZS9JdRE3Ekncd-AYElU/',
+'https://vt.tokopedia.com/t/ZS9Jd887AAD1T-gZYGK/',
+'https://vt.tokopedia.com/t/ZS9Jd8ddL4ofx-buN24/',
+'https://vt.tokopedia.com/t/ZS9Jd8BAtS2W5-tJRP9/',
+'https://vt.tokopedia.com/t/ZS9Jd8BKUqca7-TZXyS/',
+'https://vt.tokopedia.com/t/ZS9Jd8Sb4Aapm-7J0iQ/',
+'https://vt.tokopedia.com/t/ZS9Jd8UbtKmaX-OO4Pa/',
+'https://vt.tokopedia.com/t/ZS9Jd8uvHkYwq-34bHC/',
+'https://vt.tokopedia.com/t/ZS9Jd8aEw7Cms-A5W5j/',
+'https://vt.tokopedia.com/t/ZS9Jd89EhKr4G-s7pq1/',
+'https://vt.tokopedia.com/t/ZS9Jd8xEjtU6r-ZpADp/',
+'https://vt.tokopedia.com/t/ZS9JdFmmHbCsC-XzHRq/',
+'https://vt.tokopedia.com/t/ZS9JdFHF6W1sW-yzYbS/',
+'https://vt.tokopedia.com/t/ZS9JdFgUqKHEU-zJz1M/',
+'https://vt.tokopedia.com/t/ZS9JdFb31hLxh-BXaRd/',
+'https://vt.tokopedia.com/t/ZS9JdF32YD8dX-akEzk/',
+'https://vt.tokopedia.com/t/ZS9JdFvKy4Vxq-nAE3B/',
+'https://vt.tokopedia.com/t/ZS9JdPbeDK51j-azajz/',
+'https://vt.tokopedia.com/t/ZS9JdPnNTR3nD-oHRkg/',
+'https://vt.tokopedia.com/t/ZS9JdPWMq1QUD-DGUjZ/',
+'https://vt.tokopedia.com/t/ZS9JdPvWpadXd-7Hdlz/'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
