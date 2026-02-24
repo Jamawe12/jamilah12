dfb4f9cccfdbaff99862ecf561666a9350ee9bee
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-25 00:56:58 +0700

    Update blogredkeplayyviideo.js

--- blogredkeplayyviideo.js
+++ blogredkeplayyviideo.js
@@ -1,12 +1,29 @@
 var dataUrl = [
 
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-30.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-60.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-58.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-56.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-55.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-54.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-52.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-51.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-50.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-48.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-46.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-45.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-44.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-43.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-42.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-40.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-39.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-38.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-37.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-36.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-35.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-34.html',
+'https://playyviideooindoo.blogspot.com/2025/12/play-video-32.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-29.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-28.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-26.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-25.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-24.html',
-'https://playyviideooindoo.blogspot.com/2025/10/play-video-23.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-22.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-21.html',
 'https://playyviideooindoo.blogspot.com/2025/10/play-video-20.html',
@@ -22,3 +39,5 @@ var dataUrl = [
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
+
+
