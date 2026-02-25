20f7fa4fea9823183d0fc85994546e9b3bed986c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-25 14:59:41 +0700

    Create akeadster3dr5langsunggg.js

---
+++ akeadster3dr5langsunggg.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://claimsthemselves.com/bmkhx5f6?key=8fc0d28bfc53f85e46eb4e3e623d9630',
+
+  'https://claimsthemselves.com/bmkhx5f6?key=8fc0d28bfc53f85e46eb4e3e623d9630',
+
+  'https://claimsthemselves.com/bmkhx5f6?key=8fc0d28bfc53f85e46eb4e3e623d9630'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
