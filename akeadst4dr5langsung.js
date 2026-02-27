1c0583737878ef23d4a60b9ab73a9343a35786f6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-27 14:23:47 +0700

    Create akeadst4dr5langsung.js

---
+++ akeadst4dr5langsung.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://whistlemiddletrains.com/bjspmwe53?key=83cb5b5e0ad6e9ab08df6becf882af25',
+'https://whistlemiddletrains.com/bjspmwe53?key=83cb5b5e0ad6e9ab08df6becf882af25',
+'https://whistlemiddletrains.com/bjspmwe53?key=83cb5b5e0ad6e9ab08df6becf882af25'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
