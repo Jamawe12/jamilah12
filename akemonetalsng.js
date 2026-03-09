a1db3fcaaabc68b4e49f3926ece65881dc86d9cb
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-09 17:02:03 +0700

    Create akemonetalsng.js

---
+++ akemonetalsng.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://omg10.com/4/5017098',
+'https://omg10.com/4/5017098',
+'https://omg10.com/4/5017098'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
