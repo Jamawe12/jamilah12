4ff34eabdf23568e028dca0c083a17b92fec9bea
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-23 21:34:17 +0700

    Create akeadster6dr1langsungg.js

---
+++ akeadster6dr1langsungg.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://speedingdeadlyplays.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73',
+
+  'https://speedingdeadlyplays.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73',
+
+  'https://speedingdeadlyplays.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
