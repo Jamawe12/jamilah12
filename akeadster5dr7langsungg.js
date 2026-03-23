9ac8df0840cc4ea263b0e76eb55df49b661afa32
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-23 21:33:05 +0700

    Create akeadster5dr7langsungg.js

---
+++ akeadster5dr7langsungg.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://landslidegraphsystems.com/ipyk3d08dv?key=98bc3d2a4afed7b5c6eeec23965a4d54',
+
+  'https://landslidegraphsystems.com/ipyk3d08dv?key=98bc3d2a4afed7b5c6eeec23965a4d54',
+
+  'https://landslidegraphsystems.com/ipyk3d08dv?key=98bc3d2a4afed7b5c6eeec23965a4d54'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
