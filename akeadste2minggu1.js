05e0d6604af04433ed70379bb4cca5fb50dcc3a7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-31 14:43:15 +0700

    Create akeadste2minggu1.js

---
+++ akeadste2minggu1.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://undercoverhiking.com/u82wyp5z?key=8f198288674f1ed28f0b1077b76c6add',
+'https://undercoverhiking.com/u82wyp5z?key=8f198288674f1ed28f0b1077b76c6add',
+'https://undercoverhiking.com/u82wyp5z?key=8f198288674f1ed28f0b1077b76c6add'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
