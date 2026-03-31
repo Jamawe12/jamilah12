7467834e7ba5874342926e9a6bbe94572508a56a
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-31 22:23:23 +0700

    Create akeadste2minggu22.js

---
+++ akeadste2minggu22.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://www.profitablecpmratenetwork.com/qp3xdau6p0?key=749723c1a9c00882129654b901c7bf14',
+'https://www.profitablecpmratenetwork.com/qp3xdau6p0?key=749723c1a9c00882129654b901c7bf14',
+'https://www.profitablecpmratenetwork.com/qp3xdau6p0?key=749723c1a9c00882129654b901c7bf14'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
