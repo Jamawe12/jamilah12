99e15c066a94e88e3bfc0caac9b4ac784d5b0cd6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-07 10:06:27 +0700

    Delete akeadst4dr5langsung.js

--- akeadst4dr5langsung.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://whistlemiddletrains.com/bjspmwe53?key=83cb5b5e0ad6e9ab08df6becf882af25',
-'https://whistlemiddletrains.com/bjspmwe53?key=83cb5b5e0ad6e9ab08df6becf882af25',
-'https://whistlemiddletrains.com/bjspmwe53?key=83cb5b5e0ad6e9ab08df6becf882af25'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
