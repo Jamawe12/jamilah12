6317b30b75de96c056e90acb2c2085a65966ebb1
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-07 09:57:57 +0700

    Create akeadsterrmingg1.js

---
+++ akeadsterrmingg1.js
@@ -0,0 +1,17 @@
+var dataUrl = [
+
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://coldnesshoneysnort.com/ftx4mqjnu6?key=45abe4a69054461dc2189a456d40c312',
+'https://chatmylinkk.pages.dev/yThjahTTs'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
