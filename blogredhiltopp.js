3d42097bcad1d2191db64f3ad5193d76949a8b8f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-10 20:59:18 +0700

    Create blogredhiltopp.js

---
+++ blogredhiltopp.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-49.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-48.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-46.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-44.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-43.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-42.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-41.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-38.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-37.html',
+'https://chattgruppwhatsappp.blogspot.com/2026/01/grup-36.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
