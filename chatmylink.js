be011c752415d6d440ed896dfa69722fd326e05f
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-18 17:13:13 +0700

    Create chatmylink.js

---
+++ chatmylink.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://coldnesshoneysnort.com/h6gi5st6?key=dda20eb8d66854fa98f5a5e139844ac6',
+'https://coldnesshoneysnort.com/h6gi5st6?key=dda20eb8d66854fa98f5a5e139844ac6',
+'https://coldnesshoneysnort.com/h6gi5st6?key=dda20eb8d66854fa98f5a5e139844ac6'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
