4260caf17dc6aa5c801373c43e91f206d7eed193
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-20 11:42:10 +0700

    Delete akehitllads.js

--- akehitllads.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://wrathful-piano.com/bz3.Vm0SP/3VpuvcbcmnVTJXZqDa0j1HMMzgcu3BNVj/QGysLgT/U-z/N-zscP2gN/DHMl',
-'https://wrathful-piano.com/bz3.Vm0SP/3VpuvcbcmnVTJXZqDa0j1HMMzgcu3BNVj/QGysLgT/U-z/N-zscP2gN/DHMl',
-'https://wrathful-piano.com/bz3.Vm0SP/3VpuvcbcmnVTJXZqDa0j1HMMzgcu3BNVj/QGysLgT/U-z/N-zscP2gN/DHMl'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
