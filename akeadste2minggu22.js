f2705cc24b11a466350ea3cdd56de00bdfe4bed9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-20 11:49:12 +0700

    Delete akeadste2minggu22.js

--- akeadste2minggu22.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://www.profitablecpmratenetwork.com/qp3xdau6p0?key=749723c1a9c00882129654b901c7bf14',
-'https://www.profitablecpmratenetwork.com/qp3xdau6p0?key=749723c1a9c00882129654b901c7bf14',
-'https://www.profitablecpmratenetwork.com/qp3xdau6p0?key=749723c1a9c00882129654b901c7bf14'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
