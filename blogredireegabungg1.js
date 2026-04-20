a9b4ff8ed879be6fae568436b038132be5624dd6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-20 15:16:17 +0700

    Update blogredireegabungg1.js

--- blogredireegabungg1.js
+++ blogredireegabungg1.js
@@ -44,7 +44,12 @@ var dataUrl = [
 'https://playvideodood.blogspot.com/2025/11/play-video-29.html',
 'https://playvideodood.blogspot.com/2025/11/play-video-28.html',
 'https://playvideodood.blogspot.com/2025/11/play-video-27.html',
-'https://playvideodood.blogspot.com/2025/11/play-video-26.html'
+'https://playvideodood.blogspot.com/2025/11/play-video-26.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-25.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-24.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-23.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-22.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-21.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
