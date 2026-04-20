005a329a8dc5225e0ecd08b922b8ed06ed5d6484
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-20 11:32:47 +0700

    Delete akeadste2minggu1.js

--- akeadste2minggu1.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://undercoverhiking.com/u82wyp5z?key=8f198288674f1ed28f0b1077b76c6add',
-'https://undercoverhiking.com/u82wyp5z?key=8f198288674f1ed28f0b1077b76c6add',
-'https://undercoverhiking.com/u82wyp5z?key=8f198288674f1ed28f0b1077b76c6add'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
