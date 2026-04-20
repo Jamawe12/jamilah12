2cf0d6d6dafa1f864e2a80ec72cef31c67f3a09a
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-20 11:41:21 +0700

    Delete akemonettwitt.js

--- akemonettwitt.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-  'https://t.co/TuO6wKemni',
-
-  'https://t.co/mNDOlRbZdx'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
