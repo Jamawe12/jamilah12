fa62ce0002569953282192b915ce44d713369764
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-26 21:32:24 +0700

    Create akeadstf1.js

---
+++ akeadstf1.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://constellationexclusion.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73',
+'https://constellationexclusion.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73',
+'https://constellationexclusion.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
