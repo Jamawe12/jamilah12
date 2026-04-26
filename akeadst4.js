fb4e7a19d79699ad0887c9d198424016d3b54bd7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-26 21:31:26 +0700

    Create akeadst4.js

---
+++ akeadst4.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://coldnesshoneysnort.com/dtewic8a?key=e660450aae62934fc8677975a80574fd',
+'https://coldnesshoneysnort.com/dtewic8a?key=e660450aae62934fc8677975a80574fd',
+'https://coldnesshoneysnort.com/dtewic8a?key=e660450aae62934fc8677975a80574fd'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
