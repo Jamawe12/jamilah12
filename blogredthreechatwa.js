5dad32b1d9a77496805b4710d4e62d304f754a96
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-26 13:43:16 +0700

    Create blogredthreechatwa.js

---
+++ blogredthreechatwa.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+'https://chattwhatsappkuu1.blogspot.com/2026/04/chat-whatsapp-5.html',
+'https://chattwhatsappkuu1.blogspot.com/2026/04/chat-whatsapp-4.html',
+'https://chattwhatsappkuu1.blogspot.com/2026/04/chat-whatsapp-3.html',
+'https://chattwhatsappkuu1.blogspot.com/2026/04/chat-whatsapp-2.html',
+'https://chattwhatsappkuu1.blogspot.com/2026/04/chat-whatsapp-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
