58c03af4591f6e864893a77588e1ecd98a2edab8
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-04-26 14:40:03 +0700

    Create blogredthreea1.js

---
+++ blogredthreea1.js
@@ -0,0 +1,13 @@
+var dataUrl = [
+
+'https://playvideodood.blogspot.com/2025/10/play-video-18.html',
+'https://playvideodood.blogspot.com/2025/10/play-video-17.html',
+'https://playyvideoonoww1.blogspot.com/2026/04/play-video-5.html',
+'https://playyvideoonoww2.blogspot.com/2026/04/play-video-5.html',
+'https://playyvideoonoww3.blogspot.com/2026/04/play-video-5.html',
+'https://playyvideoonoww4.blogspot.com/2026/04/play-video-5.html',
+'https://playyvideoonoww5.blogspot.com/2026/04/play-video-5.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
