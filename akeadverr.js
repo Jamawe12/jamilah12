2ac866fba3720515f75a26c56a6781b51b44e5d2
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-25 14:51:15 +0700

    Create akeadverr.js

---
+++ akeadverr.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://data527.click/28f701c10cc5dbb72e27/7f77b14afc/?placementName=default',
+'https://data527.click/28f701c10cc5dbb72e27/7f77b14afc/?placementName=default',
+'https://data527.click/28f701c10cc5dbb72e27/7f77b14afc/?placementName=default'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
