c85627f86c5086349924d0fb28d7b60526934d1e
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 10:11:13 +0700

    Delete akemonetalsng.js

--- akemonetalsng.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://omg10.com/4/5017098',
-'https://omg10.com/4/5017098',
-'https://omg10.com/4/5017098'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
