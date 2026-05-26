bdf13ec94c76f3f3ce4e2a36474b9e1c234134ff
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 10:11:02 +0700

    Delete akemonetagg.js

--- akemonetagg.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://t.co/EvpDGfcEvK',
-'https://t.co/EvpDGfcEvK',
-'https://t.co/EvpDGfcEvK'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
