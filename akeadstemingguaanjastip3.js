a574cc72c39615cbd89262b6c0afd92cb3e5036b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 10:03:48 +0700

    Delete akeadstemingguaanjastip3.js

--- akeadstemingguaanjastip3.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://coldnesshoneysnort.com/fkpk6ddbk?key=497d1b2f10637acee4b21e1b2a5ba87b',
-'https://coldnesshoneysnort.com/fkpk6ddbk?key=497d1b2f10637acee4b21e1b2a5ba87b',
-'https://coldnesshoneysnort.com/fkpk6ddbk?key=497d1b2f10637acee4b21e1b2a5ba87b'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
