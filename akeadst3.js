3a4fdce1d5edf6f768d1335601e181a9d3554617
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 09:59:17 +0700

    Delete akeadst3.js

--- akeadst3.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://coldnesshoneysnort.com/atr7vd5n?key=7f92edc3f7abd602afc41966cf4fe573',
-'https://coldnesshoneysnort.com/atr7vd5n?key=7f92edc3f7abd602afc41966cf4fe573',
-'https://coldnesshoneysnort.com/atr7vd5n?key=7f92edc3f7abd602afc41966cf4fe573'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
