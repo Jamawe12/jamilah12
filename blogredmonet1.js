27f0f2f5959c62cef8487319bfb465c0cddc31e5
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 10:39:19 +0700

    Create blogredmonet1.js

---
+++ blogredmonet1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-10.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-9.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-8.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-7.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-6.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-5.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-4.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-3.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-2.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
