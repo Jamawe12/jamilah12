d3c6b44d7277528f26851e6f2844b5353188d620
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 10:09:08 +0700

    Delete akeadstf1.js

--- akeadstf1.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://constellationexclusion.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73',
-'https://constellationexclusion.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73',
-'https://constellationexclusion.com/chg8eg381k?key=a51e649e5b2463f67dc4beba75d43a73'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
