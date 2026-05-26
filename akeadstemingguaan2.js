16d65ceccd7ae53abc40d452d95c9f4bcc190774
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-26 09:59:37 +0700

    Delete akeadstemingguaan2.js

--- akeadstemingguaan2.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-
-'https://coldnesshoneysnort.com/jhgcbwvgq1?key=e48f5e324a2ccbb711e419f9ce5e2b22',
-'https://coldnesshoneysnort.com/jhgcbwvgq1?key=e48f5e324a2ccbb711e419f9ce5e2b22',
-'https://coldnesshoneysnort.com/jhgcbwvgq1?key=e48f5e324a2ccbb711e419f9ce5e2b22'
-
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
