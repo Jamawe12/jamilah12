46e432c73b908e5e7cb134dcf8d66c19a7995eb9
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-29 22:05:06 +0700

    Create akeadsttr2.js

---
+++ akeadsttr2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://impassableoffend.com/kj1jee0f?key=efe4d860564ae450371eb47d2bc147a0',
+'https://impassableoffend.com/kj1jee0f?key=efe4d860564ae450371eb47d2bc147a0',
+'https://impassableoffend.com/kj1jee0f?key=efe4d860564ae450371eb47d2bc147a0'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
