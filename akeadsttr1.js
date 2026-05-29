067e42ecd7275febdddfbc62a926c196e640365b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-29 22:04:09 +0700

    Create akeadsttr1.js

---
+++ akeadsttr1.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://impassableoffend.com/w4r4pvtwaa?key=62ac92c7c5426fdf3bf3ae0076548fa8',
+'https://impassableoffend.com/w4r4pvtwaa?key=62ac92c7c5426fdf3bf3ae0076548fa8',
+'https://impassableoffend.com/w4r4pvtwaa?key=62ac92c7c5426fdf3bf3ae0076548fa8'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
