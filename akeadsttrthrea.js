753a13cd1d3cae7e2424cc3ac85464e2f04aa338
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-05-30 21:18:19 +0700

    Create akeadsttrthrea.js

---
+++ akeadsttrthrea.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/kj1jee0f?key=efe4d860564ae450371eb47d2bc147a0',
+'https://impassableoffend.com/kj1jee0f?key=efe4d860564ae450371eb47d2bc147a0',
+'https://impassableoffend.com/kj1jee0f?key=efe4d860564ae450371eb47d2bc147a0'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
