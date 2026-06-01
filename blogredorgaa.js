8494cebc0a8dabee92d2de05f1bc8b62df45f1fb
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-01 10:11:12 +0700

    Update blogredorgaa.js

--- blogredorgaa.js
+++ blogredorgaa.js
@@ -1,25 +1,25 @@
 var dataUrl = [
 
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-13.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-12.html',
+'https://playyyvideoonoww.blogspot.com/2026/05/play-video-11.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-10.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-9.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-8.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-7.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-6.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-5.html',
-'https://playyyvideoonoww.blogspot.com/2026/05/play-video-4.html',
 'https://playyyvideoonoww.blogspot.com/2026/05/play-video-3.html',
-'https://playyyvideoonoww.blogspot.com/2026/05/play-video-2.html',
-'https://playyyvideoonoww.blogspot.com/2026/05/play-video-1.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-10.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-9.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-8.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-7.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-6.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-5.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-4.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-3.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-2.html',
-'https://viddplayvideoonow.blogspot.com/2026/04/play-video-1.html'
+'https://viddplayvideoonow.blogspot.com/2026/05/play-video-20.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-19.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-18.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-17.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-16.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-15.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-14.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-13.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-12.html',
+'https://viddplayvideoonow.blogspot.com/2026/04/play-video-11.html'
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
