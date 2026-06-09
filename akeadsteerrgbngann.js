d16e55c2671fff6066ef33ed5f108eaf69a90ee7
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-09 12:33:59 +0700

    Create akeadsteerrgbngann.js

---
+++ akeadsteerrgbngann.js
@@ -0,0 +1,31 @@
+var dataUrl = [
+
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/wm7iwsud?key=c58d4480f77de413dbf6bc98b7eaee87',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/g4xx46mk?key=6267b0244f37fda3e78ec5a13a4f64d7',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/hhv3v5wb?key=926c481290ae56139bf2cc1ff6cbb5e3',
+'https://impassableoffend.com/w4r4pvtwaa?key=62ac92c7c5426fdf3bf3ae0076548fa8'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
