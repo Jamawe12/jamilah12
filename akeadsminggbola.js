54a772cca4d5fa22b67f3bf64ca02e8c00976616
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-15 20:34:25 +0700

    Create akeadsminggbola.js

---
+++ akeadsminggbola.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+  'https://impassableoffend.com/qupsydmmj?key=df9abc2969030a4a21e1d85e30536383',
+  'https://impassableoffend.com/qupsydmmj?key=df9abc2969030a4a21e1d85e30536383',
+  'https://impassableoffend.com/qupsydmmj?key=df9abc2969030a4a21e1d85e30536383'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
