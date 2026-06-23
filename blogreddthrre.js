f0d94d03ad49055a0c3c9a637d50380ae95aa0b0
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-06-23 20:25:02 +0700

    Create blogreddthrre.js

---
+++ blogreddthrre.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-10.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-9.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-8.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-7.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-6.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-5.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-4.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-3.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-2.html',
+'https://playyvideoslicee.blogspot.com/2026/06/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
