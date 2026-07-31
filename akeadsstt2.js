f54beb3e0f550d60c955a8c7c1a7cbf588f2cb05
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-07-31 07:14:11 +0700

    Create akeadsstt2.js

---
+++ akeadsstt2.js
@@ -0,0 +1,20 @@
+var dataUrl = [
+
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/frik9vyd?key=13e375eb7e8fd8aa1f5cb283b4057627',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/crya9k2x6?key=c3ddf144181d5fbd0dc73d00e1c280ac',
+'https://impassableoffend.com/dk50270se?key=8c8632aee311b483dd1ad5a814003c94',
+'https://impassableoffend.com/dk50270se?key=8c8632aee311b483dd1ad5a814003c94',
+'https://impassableoffend.com/dk50270se?key=8c8632aee311b483dd1ad5a814003c94',
+'https://impassableoffend.com/wwam61979v?key=7c28ae78a5212632b867261f92ea162a',
+'https://impassableoffend.com/wwam61979v?key=7c28ae78a5212632b867261f92ea162a',
+'https://impassableoffend.com/wwam61979v?key=7c28ae78a5212632b867261f92ea162a'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
