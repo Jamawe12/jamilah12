af30c491d8018d8b43de742fb9cb42da31427597
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-08-07 14:34:30 +0700

    Create aakeadstt11.js

---
+++ aakeadstt11.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://impassableoffend.com/cpvev1nj?key=d5c7343878687e73c517d3fc718bec2e',
+'https://impassableoffend.com/cpvev1nj?key=d5c7343878687e73c517d3fc718bec2e',
+'https://impassableoffend.com/cpvev1nj?key=d5c7343878687e73c517d3fc718bec2e',
+'https://impassableoffend.com/x0iwg23y3b?key=5bc14c88c99d62a645acbec488b45865',
+'https://impassableoffend.com/x0iwg23y3b?key=5bc14c88c99d62a645acbec488b45865',
+'https://impassableoffend.com/x0iwg23y3b?key=5bc14c88c99d62a645acbec488b45865'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
