bd6de519704cf5d1dcfd9d70a4203206771bdd42
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-08-23 12:51:34 +0700

    Create keblogreddmingg1ke3.js

---
+++ keblogreddmingg1ke3.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-20.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-19.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-18.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-17.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-16.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-15.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-14.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-13.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-12.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-11.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-10.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-9.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-8.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-7.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-6.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-5.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-4.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-3.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-2.html',
+'https://playvideooonowplay.blogspot.com/2026/08/play-video-1.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
